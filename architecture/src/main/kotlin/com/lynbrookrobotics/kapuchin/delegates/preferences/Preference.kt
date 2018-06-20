23c461beba4b6c3eb2c4270ae119a31282defc54
Kunal Sheth <kunal@kunalsheth.info>
2018-06-20 14:25:08 -0700

    Add nameSuffix option to Preference class

--- architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/delegates/preferences/Preference.kt
+++ architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/delegates/preferences/Preference.kt
@@ -1,12 +1,12 @@
 package com.lynbrookrobotics.kapuchin.delegates.preferences
 
-import com.lynbrookrobotics.kapuchin.subsystems.Named
-import com.lynbrookrobotics.kapuchin.delegates.WithEventLoop
 import com.lynbrookrobotics.kapuchin.delegates.DelegateProvider
+import com.lynbrookrobotics.kapuchin.delegates.WithEventLoop
+import com.lynbrookrobotics.kapuchin.subsystems.Named
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-class Preference<Value>(private val fallback: Value, private val get: (String, Value) -> Value) : WithEventLoop, DelegateProvider<Named, Value> {
+class Preference<Value>(private val fallback: Value, private val get: (String, Value) -> Value, private val nameSuffix: String = "") : WithEventLoop, DelegateProvider<Named, Value> {
 
     private lateinit var name: String
     private var value: Value? = null
@@ -18,7 +18,7 @@ class Preference<Value>(private val fallback: Value, private val get: (String, V
     }
 
     override fun provideDelegate(thisRef: Named, prop: KProperty<*>): ReadOnlyProperty<Named, Value> {
-        name = namePreference(thisRef, prop)
+        name = namePreference(thisRef, prop) + nameSuffix
 
         return object : ReadOnlyProperty<Named, Value> {
             override fun getValue(thisRef: Named, property: KProperty<*>) = value ?: get(name, fallback)
