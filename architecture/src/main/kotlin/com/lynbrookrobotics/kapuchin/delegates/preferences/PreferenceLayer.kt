e0cff01b8d0f3e74286da04a50ba328c75da0e26
Kunal Sheth <kunal@kunalsheth.info>
2018-06-20 17:51:52 -0700

    Support hot reloading of layered preferences

--- architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/delegates/preferences/PreferenceLayer.kt
+++ architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/delegates/preferences/PreferenceLayer.kt
@@ -8,25 +8,25 @@ import kotlin.reflect.KProperty
 
 class PreferenceLayer<Value>(
         private val nameSuffix: String = "",
-        private val get: Named.() -> Value
+        private val construct: Named.() -> (() -> Value)
 ) : WithEventLoop, DelegateProvider<Named, Value> {
 
-    private lateinit var named: Named
+    private lateinit var get: () -> Value
     private var value: Value? = null
 
     override fun update() {
-        if (this::named.isInitialized) {
-            value = get(named)
+        if (this::get.isInitialized) {
+            value = get()
         }
     }
 
     override fun provideDelegate(thisRef: Named, prop: KProperty<*>): ReadOnlyProperty<Named, Value> {
-        named = object : Named {
+        get = object : Named {
             override val name = namePreference(thisRef, prop) + nameSuffix
-        }
+        }.run(construct)
 
         return object : ReadOnlyProperty<Named, Value> {
-            override fun getValue(thisRef: Named, property: KProperty<*>) = value ?: get(named)
+            override fun getValue(thisRef: Named, property: KProperty<*>) = value!!
         }
     }
 }
