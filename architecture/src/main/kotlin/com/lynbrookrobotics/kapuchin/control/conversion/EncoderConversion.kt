de036859052923f2df727ecfe56b829771b5b646
Kunal Sheth <kunal@kunalsheth.info>
2018-06-22 12:25:19 -0700

    Cleanup EncoderConversion fullTurn value

--- architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/control/conversion/EncoderConversion.kt
+++ architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/control/conversion/EncoderConversion.kt
@@ -6,17 +6,16 @@ class EncoderConversion(ticks: Tick, perRevolution: Angle) {
     private val ticksPerRevolution = ticks / perRevolution
 
     fun ticks(x: AngularAbsement) = ticks(x / t) * t
-    fun ticks(x: Radian): Tick = (x / fullTurn) * ticksPerRevolution
+    fun ticks(x: Radian): Tick = (x / 1.Turn) * ticksPerRevolution
     fun ticks(x: AngularVelocity) = ticks(x * t) / t
     fun ticks(x: AngularAcceleration) = ticks(x * t) / t
 
     fun angle(x: Time): AngularAbsement = t * angle((x / t))
-    fun angle(x: Tick): Radian = (x / ticksPerRevolution) * fullTurn
+    fun angle(x: Tick): Radian = (x / ticksPerRevolution) * 1.Turn
     fun angle(x: Frequency) = angle((x * t)) / t
     fun angle(x: `T⁻²`) = angle(x * t) / t
 
     companion object {
         private val t = 1.Second
-        private val fullTurn: Angle = 360.Degree
     }
 }
