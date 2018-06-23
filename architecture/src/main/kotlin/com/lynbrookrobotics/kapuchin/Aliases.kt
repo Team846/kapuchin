8616d5f2285d054e6cf2de06aedc0ca85573b6e0
Kunal Sheth <kunal@kunalsheth.info>
2018-06-22 17:36:15 -0700

    Create component init DSL (#10)

--- architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/Aliases.kt
+++ architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/Aliases.kt
@@ -6,23 +6,43 @@ import info.kunalsheth.units.generated.Quantity
 typealias Comp = Component<*, *, *>
 typealias Quan<Q> = Quantity<Q, *, *>
 
-infix fun <A, B> ((A) -> B).dependsOn(f: () -> A?): B? =
-        try {
-            this(f()!!)
-        } catch (t: Throwable) {
-            null
-        }
+fun <R> (() -> R).safeCall() = try {
+    this()
+} catch (t: Throwable) {
+    null
+}
 
-infix fun <A, B, C> ((A, B) -> C).dependsOn(f: Pair<() -> A?, B?>): C? =
-        try {
-            this(f.first()!!, f.second!!)
-        } catch (t: Throwable) {
-            null
-        }
+private fun allNonNull(vararg all: Any?) = all.all { it != null }
 
-infix fun <A, B, C, D> ((A, B, C) -> D).dependsOn(f: Pair<Pair<() -> A?, B?>, C?>): D? =
-        try {
-            this(f.first.first()!!, f.first.second!!, f.second!!)
-        } catch (t: Throwable) {
-            null
-        }
+infix fun <A, B> A.with(that: B) = this to that
+infix fun <A, R> A?.creates(block: (A) -> R): R? = this?.let(block)
+
+infix fun <A, B, R> Pair<A?, B?>.creates(block: (A, B) -> R): R? {
+    val (a, b) = this
+    return if (allNonNull(a, b)) block(a!!, b!!)
+    else null
+}
+
+infix fun <A, B, C, R> Pair<Pair<A?, B?>, C?>.creates(block: (A, B, C) -> R): R? {
+    val (ab, c) = this
+    val (a, b) = ab
+    return if (allNonNull(a, b, c)) block(a!!, b!!, c!!)
+    else null
+}
+
+infix fun <A, B, C, D, R> Pair<Pair<Pair<A?, B?>, C?>, D?>.creates(block: (A, B, C, D) -> R): R? {
+    val (abc, d) = this
+    val (ab, c) = abc
+    val (a, b) = ab
+    return if (allNonNull(a, b, c, d)) block(a!!, b!!, c!!, d!!)
+    else null
+}
+
+infix fun <A, B, C, D, E, R> Pair<Pair<Pair<Pair<A?, B?>, C?>, D?>, E?>.creates(block: (A, B, C, D, E) -> R): R? {
+    val (abcd, e) = this
+    val (abc, d) = abcd
+    val (ab, c) = abc
+    val (a, b) = ab
+    return if (allNonNull(a, b, c, d, e)) block(a!!, b!!, c!!, d!!, e!!)
+    else null
+}
