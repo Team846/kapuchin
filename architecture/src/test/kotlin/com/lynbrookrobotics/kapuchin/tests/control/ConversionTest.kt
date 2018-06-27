cf2cf07a92d497376649d1912f2516e0475c94d6
Kunal Sheth <kunal@kunalsheth.info>
2018-06-27 12:47:05 -0700

    Test native conversion linearity

--- architecture/src/test/kotlin/com/lynbrookrobotics/kapuchin/tests/control/ConversionTest.kt
+++ architecture/src/test/kotlin/com/lynbrookrobotics/kapuchin/tests/control/ConversionTest.kt
@@ -4,6 +4,8 @@ import com.lynbrookrobotics.kapuchin.control.conversion.EncoderConversion
 import com.lynbrookrobotics.kapuchin.control.conversion.GearTrain
 import com.lynbrookrobotics.kapuchin.control.conversion.OffloadedNativeConversion
 import com.lynbrookrobotics.kapuchin.control.conversion.WheelConversion
+import com.lynbrookrobotics.kapuchin.control.loops.Gain
+import com.lynbrookrobotics.kapuchin.control.loops.pid.PidGains
 import com.lynbrookrobotics.kapuchin.tests.`is equal to?`
 import com.lynbrookrobotics.kapuchin.tests.anyDouble
 import com.lynbrookrobotics.kapuchin.tests.anyInt
@@ -14,7 +16,7 @@ class ConversionTest {
     private val t = 1.Second
 
     @Test
-    fun `encoder ticks and angle methods should be inverses`() {
+    fun `encoder ticks and angle methods are inverses`() {
         anyInt.filter { it != 0 }.map { resolution -> EncoderConversion(resolution.Tick, 360.Degree) }
                 .forEach { conversion ->
                     anyDouble.map { it.Tick }.forEach { x ->
@@ -33,7 +35,7 @@ class ConversionTest {
     }
 
     @Test
-    fun `wheel length and angle methods should be inverses`() {
+    fun `wheel length and angle methods are inverses`() {
         anyInt.filter { it > 0 }
                 .map { radius -> WheelConversion(radius.Inch) }
                 .forEach { conversion ->
@@ -53,7 +55,7 @@ class ConversionTest {
     }
 
     @Test
-    fun `talon real and native methods should be inverses`() {
+    fun `offloaded real and native methods are inverses`() {
         anyInt.filter { it != 0 }.map { resolution -> OffloadedNativeConversion(1023, 12.Volt, resolution, 8.46.Metre) }
                 .forEach { conversion ->
                     anyDouble.map { it.Foot }.forEach { x ->
@@ -66,7 +68,34 @@ class ConversionTest {
     }
 
     @Test
-    fun `gears input and output methods should be inverses`() {
+    fun `offloaded native methods are linear`() {
+        anyInt.filter { it != 0 }.map { resolution -> OffloadedNativeConversion(1023, 12.Volt, resolution, 8.46.Metre) }
+                .forEach { conversion ->
+                    anyDouble.map { it.Foot }.forEach { x ->
+                        conversion.native(-x) * 2 `is equal to?` -conversion.native(x * 2)
+                        conversion.native(Gain(20.Volt, x)) `is equal to?` conversion.native(Gain(10.Volt, x)) * 2
+
+                        val ix = x * 1.Second
+                        conversion.native(-ix) * 2 `is equal to?` -conversion.native(ix * 2)
+                        conversion.native(Gain(20.Volt, ix)) `is equal to?` conversion.native(Gain(10.Volt, ix)) * 2
+
+                        val dx = x / 1.Second
+                        conversion.native(-dx) * 2 `is equal to?` -conversion.native(dx * 2)
+                        conversion.native(Gain(20.Volt, dx)) `is equal to?` conversion.native(Gain(10.Volt, dx)) * 2
+
+                        val ddx = dx / t
+                        conversion.native(-ddx) * 2 `is equal to?` -conversion.native(ddx * 2)
+                        conversion.native(Gain(20.Volt, ddx)) `is equal to?` conversion.native(Gain(10.Volt, ddx)) * 2
+                    }
+
+                    anyDouble.map { it.Volt }.forEach { x ->
+                        conversion.native(-x) * 2 `is equal to?` -conversion.native(x * 2)
+                    }
+                }
+    }
+
+    @Test
+    fun `gears input and output methods are inverses`() {
         anyInt.filter { it > 0 }.forEach { input ->
             anyInt.filter { it > 0 }.forEach { idlers ->
                 anyInt.filter { it > 0 }
