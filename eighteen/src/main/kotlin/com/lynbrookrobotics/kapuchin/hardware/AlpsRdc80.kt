09536a83e9701a13177bf78ae655256300ff97d3
Kunal Sheth <kunal@kunalsheth.info>
2018-07-31 10:50:43 -0700

    Add loop-around logic to AlpsRdc80

--- eighteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/hardware/AlpsRdc80.kt
+++ eighteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/hardware/AlpsRdc80.kt
@@ -1,8 +1,8 @@
 package com.lynbrookrobotics.kapuchin.hardware
 
 import com.lynbrookrobotics.kapuchin.control.plusOrMinus
-import com.lynbrookrobotics.kapuchin.logging.withDecimals
 import info.kunalsheth.units.generated.Angle
+import info.kunalsheth.units.generated.Degree
 import info.kunalsheth.units.generated.Turn
 import kotlin.math.absoluteValue
 
@@ -11,14 +11,27 @@ class AlpsRdc80(private val phase: Angle) : (Double, Double) -> Angle {
     private val readableRange = 0 plusOrMinus 0.7
     private val halfTurn = 0.5.Turn
 
+    private var loopAround = 0.Turn
+    private var wasPositive = false
+    private var wasNegative = false
+    private val posLoopRng = 160.Degree..180.Degree
+    private val negLoopRng = -180.Degree..-160.Degree
+
     override fun invoke(a: Double, b: Double): Angle {
-        val angA = (halfTurn * a) % halfTurn
-        val angB = (halfTurn * b + phase) % halfTurn
+        val angA = (halfTurn * (a + 1)) % 1.Turn - halfTurn
+        val angB = (halfTurn * (b + 1) + phase) % 1.Turn - halfTurn
 
         val weightA = weight(a)
         val weightB = weight(b)
 
-        return (angA * weightA + angB * weightB) / (weightA + weightB)
+        val angle = (angA * weightA + angB * weightB) / (weightA + weightB)
+
+        if (wasNegative && angle in posLoopRng) loopAround -= 1.Turn
+        if (wasPositive && angle in negLoopRng) loopAround += 1.Turn
+        wasPositive = angle in posLoopRng
+        wasNegative = angle in negLoopRng
+
+        return angle + loopAround
     }
 
     private fun weight(x: Double) =
