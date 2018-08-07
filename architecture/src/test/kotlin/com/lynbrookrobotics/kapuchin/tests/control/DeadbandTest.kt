676fa8f2d467718f540cc4b08519c2420b974f6d
Kunal Sheth <kunal@kunalsheth.info>
2018-08-06 21:34:38 -0700

    Make deadband test functions members of the DeadbandTest class

--- architecture/src/test/kotlin/com/lynbrookrobotics/kapuchin/tests/control/DeadbandTest.kt
+++ architecture/src/test/kotlin/com/lynbrookrobotics/kapuchin/tests/control/DeadbandTest.kt
@@ -9,41 +9,43 @@ import com.lynbrookrobotics.kapuchin.tests.anyDouble
 import info.kunalsheth.units.generated.Percent
 import kotlin.test.Test
 
-@Test
-fun `vertical deadbands are mapped correctly`() {
-    anyDouble.filter { it > 0 && it < 100 }.forEach {
-        val mapping = VerticalDeadband(it.Percent, 100.Percent)
-        val tol = 0.01.Percent
+class DeadbandTest {
+    @Test
+    fun `vertical deadbands are mapped correctly`() {
+        anyDouble.filter { it > 0 && it < 100 }.forEach {
+            val mapping = VerticalDeadband(it.Percent, 100.Percent)
+            val tol = 0.01.Percent
 
-        mapping(0.0.Percent) `is within?` (0.0.Percent withToleranceOf tol)
-        mapping(tol / 2) `is within?` (it.Percent withToleranceOf tol)
-        mapping(100.Percent) `is within?` (100.Percent withToleranceOf tol)
-        mapping(-tol / 2) `is within?` (-it.Percent withToleranceOf tol)
-        mapping(-100.Percent) `is within?` (-100.Percent withToleranceOf tol)
+            mapping(0.0.Percent) `is within?` (0.0.Percent withToleranceOf tol)
+            mapping(tol / 2) `is within?` (it.Percent withToleranceOf tol)
+            mapping(100.Percent) `is within?` (100.Percent withToleranceOf tol)
+            mapping(-tol / 2) `is within?` (-it.Percent withToleranceOf tol)
+            mapping(-100.Percent) `is within?` (-100.Percent withToleranceOf tol)
+        }
     }
-}
 
-@Test
-fun `horizontal deadbands are mapped correctly`() {
-    anyDouble.filter { it > 0 && it < 100 }.forEach {
-        val mapping = HorizontalDeadband(it.Percent, 100.Percent)
-        val tol = 0.01.Percent
+    @Test
+    fun `horizontal deadbands are mapped correctly`() {
+        anyDouble.filter { it > 0 && it < 100 }.forEach {
+            val mapping = HorizontalDeadband(it.Percent, 100.Percent)
+            val tol = 0.01.Percent
 
-        mapping(0.0.Percent) `is within?` (0.0.Percent withToleranceOf tol)
-        mapping(it.Percent / 2) `is within?` (0.0.Percent withToleranceOf tol)
-        mapping(100.Percent) `is within?` (100.Percent withToleranceOf tol)
-        mapping(-it.Percent / 2) `is within?` (0.0.Percent withToleranceOf tol)
-        mapping(-100.Percent) `is within?` (-100.Percent withToleranceOf tol)
+            mapping(0.0.Percent) `is within?` (0.0.Percent withToleranceOf tol)
+            mapping(it.Percent / 2) `is within?` (0.0.Percent withToleranceOf tol)
+            mapping(100.Percent) `is within?` (100.Percent withToleranceOf tol)
+            mapping(-it.Percent / 2) `is within?` (0.0.Percent withToleranceOf tol)
+            mapping(-100.Percent) `is within?` (-100.Percent withToleranceOf tol)
+        }
     }
-}
 
-@Test
-fun `horizontal deadband is the inverse of vertical deadband`() {
-    anyDouble.filter { it > 0 && it < 100 }.forEach {
-        val horizontal = HorizontalDeadband(it.Percent, 100.Percent)
-        val vertical = VerticalDeadband(it.Percent, 100.Percent)
-        anyDouble.forEach {
-            horizontal(vertical(it.Percent)) `is equal to?` it.Percent
+    @Test
+    fun `horizontal deadband is the inverse of vertical deadband`() {
+        anyDouble.filter { it > 0 && it < 100 }.forEach {
+            val horizontal = HorizontalDeadband(it.Percent, 100.Percent)
+            val vertical = VerticalDeadband(it.Percent, 100.Percent)
+            anyDouble.forEach {
+                horizontal(vertical(it.Percent)) `is equal to?` it.Percent
+            }
         }
     }
 }
