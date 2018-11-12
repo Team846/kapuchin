26d91a85811ea012e3d26a2884190e913d8e9163
Kunal Sheth <kunal@kunalsheth.info>
2018-11-12 10:10:02 -0800

    Setup dokka

--- architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/control/conversion/deadband/HorizontalDeadband.kt
+++ architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/control/conversion/deadband/HorizontalDeadband.kt
@@ -1,8 +1,23 @@
 package com.lynbrookrobotics.kapuchin.control.conversion.deadband
 
-import info.kunalsheth.units.generated.*
 import com.lynbrookrobotics.kapuchin.control.div
+import info.kunalsheth.units.generated.Quan
 
+/**
+ * Maps inputs close to zero to zero
+ *
+ * Often used on joystick input to prevent drifting
+ *
+ * @author Alvyn, Kunal
+ * @see [VerticalDeadband]
+ *
+ * @param[Q] the type of input and output
+ * @param[xIntercept] must be greater than zero
+ * @param[max] must be greater than zero
+ *
+ * @property[xIntercept] the maximum input that will still be mapped to zero
+ * @property[max] the value at which the input is equal to output
+ */
 class HorizontalDeadband<Q : Quan<Q>>(val xIntercept: Q, val max: Q) : (Q) -> Q {
 
     private val slope = max / (max - xIntercept)
