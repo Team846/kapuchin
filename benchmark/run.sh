41c26e661aacf47164d48ea62882f3a1b7b4c89b
Kunal Sheth <kunal@kunalsheth.info>
2018-11-24 09:52:34 -0800

    Write benchmarking script

---
+++ benchmark/run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 0 ]]
+then
+    echo "Benchmark name not specified"
+    exit
+fi
+
+KAPUCHIN="$PWD"
+BENCHMARKS="benchmark"
+SLEEP_TIME=200
+
+osascript -e 'display notification "Deploying Benchmark" with title "Kapuchin Realtime Tuning"'
+
+cd "$KAPUCHIN"
+./gradlew :eighteen:deploy
+ssh -t admin@roboRIO-846-FRC.local reboot
+
+osascript -e 'display notification "Running Benchmark" with title "Kapuchin Realtime Tuning"'
+
+cd "$BENCHMARKS"
+mkdir "$1"
+sleep "$SLEEP_TIME"
+scp "lvuser@roboRIO-846-FRC.local:/tmp/*.csv" "$1"
+scp "lvuser@roboRIO-846-FRC.local:~/FRC_UserProgram.log" "$1"
+
+osascript -e 'display notification "Finished Benchmark" with title "Kapuchin Realtime Tuning"'
+
+cp GRAPHS.xlsx "$1"
+open "$1/*.csv"
