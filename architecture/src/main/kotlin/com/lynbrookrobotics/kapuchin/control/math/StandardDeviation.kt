166469eaad33ad4f37c4aef84d322e7ee7a82698
Kunal Sheth <kunal@kunalsheth.info>
2018-11-24 14:20:11 -0800

    Test standard deviation classes

--- architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/control/math/StandardDeviation.kt
+++ architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/control/math/StandardDeviation.kt
@@ -2,26 +2,31 @@ package com.lynbrookrobotics.kapuchin.control.math
 
 import com.lynbrookrobotics.kapuchin.control.data.Delay
 import info.kunalsheth.units.generated.Quan
+import kotlin.math.absoluteValue
 import kotlin.math.sqrt
 
 class FiniteStandardDeviation<Q : Quan<Q>, Q2 : Quan<Q2>>(
         private val times: (Q, Q) -> Q2,
-        private val zero: Q,
+        private val init: Q,
         bufferSize: Int
 ) : (Q) -> Q {
 
-    private val delay = Delay<Q>(bufferSize)
+    private val delay = Delay<Q>(bufferSize).also { it(init) }
 
-    private var sum = zero
-    private var sqSum = times(zero, zero)
-    private var n = 0
+    private var sum = init
+    private var sqSum = times(init, init)
+    private var n = 1
 
     override operator fun invoke(value: Q): Q {
-        val delayed = delay(value) ?: zero
-        n += if (delayed === zero) 1 else 0
+        sum += value
+        sqSum += times(value, value)
 
-        sum += value - delayed
-        sqSum += times(value, value) - times(delayed, delayed)
+        val delayed = delay(value)
+        if (delayed == null) n++
+        else {
+            sum -= delayed
+            sqSum -= times(delayed, delayed)
+        }
 
         return stdev(times, n, sum, sqSum)
     }
@@ -29,17 +34,17 @@ class FiniteStandardDeviation<Q : Quan<Q>, Q2 : Quan<Q2>>(
 
 class InfiniteStandardDeviation<Q : Quan<Q>, Q2 : Quan<Q2>>(
         private val times: (Q, Q) -> Q2,
-        private val zero: Q
+        private val init: Q
 ) : (Q) -> Q {
 
-    private var sum = zero
-    private var sqSum = times(zero, zero)
-    private var n = 0
+    private var sum = init
+    private var sqSum = times(init, init)
+    private var n = 1
 
     override operator fun invoke(value: Q): Q {
-        n++
         sum += value
         sqSum += times(value, value)
+        n++
 
         return stdev(times, n, sum, sqSum)
     }
@@ -51,7 +56,7 @@ private fun <Q : Quan<Q>, Q2 : Quan<Q2>> stdev(times: (Q, Q) -> Q2, n: Int, sum:
     val nxBarSq = times(xBar, xBar) * n // n̄x²
 
     val sigmaSq = (sqSum - xBarSum2 + nxBarSq) / (n - 1)
-    val sigma = sum.new(sqrt(sigmaSq.siValue))
+    val sigma = sum.new(sqrt(sigmaSq.siValue.absoluteValue))  // `.absoluteValue` because of floating point error
 
     return sigma
 }
