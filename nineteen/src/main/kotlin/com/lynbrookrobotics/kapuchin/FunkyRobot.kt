f9c770731c54072076e3a4c56dd147ffb638593d
Kunal Sheth <kunal@kunalsheth.info>
2019-01-06 13:10:47 -0800

    Make 2019 module

---
+++ nineteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/FunkyRobot.kt
@@ -0,0 +1,62 @@
+package com.lynbrookrobotics.kapuchin
+
+import com.lynbrookrobotics.kapuchin.routines.Routine.Companion.runWhile
+import com.lynbrookrobotics.kapuchin.timing.clock.EventLoop
+import com.lynbrookrobotics.kapuchin.timing.currentTime
+import com.lynbrookrobotics.kapuchin.timing.scope
+import edu.wpi.first.wpilibj.RobotBase
+import edu.wpi.first.hal.HAL
+import info.kunalsheth.units.generated.Second
+import info.kunalsheth.units.math.milli
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class FunkyRobot : RobotBase() {
+    override fun startCompetition() {
+        println("Kapuchin Run ID ${System.currentTimeMillis() / 1000 - 1514000000}")
+
+        val classloading = loadClasses()
+
+//        val subsystems = Subsystems.init()
+
+        runBlocking { classloading.join() }
+
+        HAL.observeUserProgramStarting()
+
+        val eventLoopPeriod = 20.milli(Second)
+
+        val doNothing = scope.launch { }
+        var currentJob: Job = doNothing
+
+        while (true) {
+            Thread.yield()
+            m_ds.waitForData(eventLoopPeriod.Second)
+
+            EventLoop.tick(currentTime)
+
+            if (!currentJob.isActive) {
+                currentJob = doNothing
+            }
+        }
+    }
+
+    private fun loadClasses() = scope.launch {
+        val classNameRegex = """\[Loaded ([\w.$]+) from .+]""".toRegex()
+        Thread.currentThread()
+                .contextClassLoader
+                .getResourceAsStream("com/lynbrookrobotics/kapuchin/preload")
+                .bufferedReader()
+                .lineSequence()
+                .filter { it.matches(classNameRegex) }
+                .map { it.replace(classNameRegex, "$1") }
+                .forEach {
+                    launch {
+                        try {
+                            Class.forName(it)
+                        } catch (t: Throwable) {
+                        }
+                    }
+                }
+    }
+}
