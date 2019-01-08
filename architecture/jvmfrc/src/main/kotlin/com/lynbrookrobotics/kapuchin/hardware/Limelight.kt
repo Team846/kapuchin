a38ca455f1667852b22f27b77266962f74a02598
nikwalia <nikash.walia@gmail.com>
2019-01-08 10:58:04 -0800

    added docs, condensed, stylistic changes

--- architecture/jvmfrc/src/main/kotlin/com/lynbrookrobotics/kapuchin/hardware/Limelight.kt
+++ architecture/jvmfrc/src/main/kotlin/com/lynbrookrobotics/kapuchin/hardware/Limelight.kt
@@ -8,26 +8,43 @@ import com.lynbrookrobotics.kapuchin.timing.Priority
 import edu.wpi.first.networktables.NetworkTableInstance
 import info.kunalsheth.units.generated.*
 import info.kunalsheth.units.math.milli
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
+/**
+ * System that retrieves data from the Limelight and calculates the angle to target while accounting for latency
+ *
+ * @author Nikash Walia
+ */
 class LimelightSystem: SubsystemHardware<LimelightSystem, Nothing>() {
     override val name: String = "Limelight"
     override val priority = Priority.Lowest
     override val period: Time = 20.milli(Second)
     override val syncThreshold: Time = 3.milli(Second)
 
-    private val cameraIsVertical by pref(true)
     private val cameraAngleRelativeToFront: Angle by pref(0, Degree)
-
     private val table = NetworkTableInstance.getDefault().getTable("/limelight")
-    private fun <Input> s(f: () -> Input) = sensor { f() stampWith it - Millisecond(table.getEntry("tl").getDouble(0.0)) }
 
-    private val angleToTarget = s {
-        if (table.getEntry("tv").getDouble(0.0) != 1.0) {
-            null
-        } else if (cameraIsVertical) {
-            cameraAngleRelativeToFront + Degree(table.getEntry("tx").getDouble(0.0))
-        } else {
-            cameraAngleRelativeToFront + Degree(table.getEntry("ty").getDouble(0.0))
+    /**
+     * Helper method- retrieves entries from Limelight network tables
+     *
+     * @param key the key for which the value must be retrieved
+     */
+    private fun getEntry(key: String) = table.getEntry(key).getDouble(0.0).roundToInt()
+
+    /**
+     * Sensor that calculates the angle to target, taking into account camera mounting. Nullable- if there is no target
+     */
+    private val angletoTarget = sensor {
+        { when (getEntry("tv")) {
+            0 -> null
+            else -> { if (getEntry("ts").absoluteValue < 2) { // tolerance
+                cameraAngleRelativeToFront + getEntry("tx").Degree
+                } else {
+                cameraAngleRelativeToFront + getEntry("ty").Degree
+                }
+            }
         }
+        } stampWith  it - getEntry("tl").milli(Second)
     }
 }
