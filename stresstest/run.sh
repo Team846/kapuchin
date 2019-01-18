335143f33d56392bfce4dde168f4e43b6b66d6e8
Kunal Sheth <kunal@kunalsheth.info>
2019-01-17 22:57:58 -0800

    Write stress testing util

---
+++ stresstest/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 0 ]]
+then
+    echo "Stress test task not specified"
+    exit
+fi
+
+osascript -e 'display notification "Running Stress Test" with title "Kapuchin Stress Testing"'
+
+i="0"
+while
+$@
+do :
+i=$[$i+1]
+done
+
+osascript -e 'display notification "Stress Test Failed" with title "Kapuchin Stress Testing"'
+echo "Test failed after $i trials."
