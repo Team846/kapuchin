6f18924d92169ef53d23f920b2f225b8323e4af2
Kunal Sheth <kunal@kunalsheth.info>
2019-01-30 05:20:28 -0800

    Use inline TicksToSerialValue instead of TwoSided<T> to lower GC pressure

---
+++ architecture/jvmfrc/src/main/kotlin/com/lynbrookrobotics/kapuchin/hardware/tickstoserial/TicksToSerialValue.kt
@@ -0,0 +1,22 @@
+package com.lynbrookrobotics.kapuchin.hardware.ticksToSerial
+
+inline class TicksToSerialValue(private val data: Int) {
+    val left: Int
+        get() {
+            val left = data ushr 4 and 0x0F
+            val absvl = left and 0b0111
+            val signl = left and 0b1000 ushr 3
+            return if (signl == 1) absvl else -absvl
+        }
+
+    val right: Int
+        get() {
+            val right = data ushr 0 and 0x0F
+            val absvr = right and 0b0111
+            val signr = right and 0b1000 ushr 3
+            return if (signr == 1) absvr else -absvr
+        }
+
+    fun component1() = left
+    fun component2() = right
+}
