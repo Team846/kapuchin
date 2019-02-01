7382f7ee7f84c39b7391f8ef313d3c100e574de1
Kunal Sheth <kunal@kunalsheth.info>
2019-01-31 17:07:33 -0800

    Write thread dumper util

---
+++ simulated/src/test/kotlin/com/lynbrookrobotics/kapuchin/tests/routine/Threading.kt
@@ -0,0 +1,17 @@
+package com.lynbrookrobotics.kapuchin.tests.routine
+
+import com.lynbrookrobotics.kapuchin.logging.Level
+import com.lynbrookrobotics.kapuchin.logging.Named
+import com.lynbrookrobotics.kapuchin.logging.log
+
+fun threadDumpOnFailiure(f: () -> Unit) {
+    Named("Thread Dumper").apply {
+        try {
+            f()
+        } catch (t: Throwable) {
+            Thread.getAllStackTraces().forEach { t, u ->
+                log(Level.Error, u) { t.name }
+            }
+        }
+    }
+}
