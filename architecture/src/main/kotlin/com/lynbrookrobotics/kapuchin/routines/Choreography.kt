40bed9121024df04e9dd1c4e1d1d81e8dc87a601
Kunal Sheth <kunal@kunalsheth.info>
2019-02-09 10:51:23 -0800

    Move choreo functions from Routine.kt to Choreo....kt

--- architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/routines/Choreography.kt
+++ architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/routines/Choreography.kt
@@ -3,10 +3,15 @@ package com.lynbrookrobotics.kapuchin.routines
 import com.lynbrookrobotics.kapuchin.logging.*
 import com.lynbrookrobotics.kapuchin.logging.Level.*
 import com.lynbrookrobotics.kapuchin.subsystems.*
+import com.lynbrookrobotics.kapuchin.timing.clock.*
 import info.kunalsheth.units.generated.*
+import info.kunalsheth.units.math.*
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
+
+private typealias Block = suspend CoroutineScope.() -> Unit
 
 /**
  * Represents a sequence of subsystem routines
@@ -23,7 +28,7 @@ import kotlinx.coroutines.launch
  */
 fun CoroutineScope.startRoutine(
         name: String,
-        setup: FreeSensorScope.() -> suspend CoroutineScope.() -> Unit
+        setup: FreeSensorScope.() -> Block
 ) = launch {
     val named = Named(name)
     val sensorScope = FreeSensorScope()
@@ -48,4 +53,59 @@ fun CoroutineScope.startRoutine(
  * @param Time loop start time
  * @return value to write to hardware or `null` to end the routine
  */
-fun CoroutineScope.choreography(controller: suspend CoroutineScope.() -> Unit) = controller
+fun CoroutineScope.choreography(controller: Block) = controller
+
+/**
+ * Create a new coroutine running all `routines` in parallel
+ *
+ * @param blocks collection of functions to run in parallel
+ * @return parent coroutine of the running routines
+ */
+fun CoroutineScope.launchAll(vararg blocks: Block) = startRoutine("launchAll") {
+    choreography {
+        blocks.forEach { launch { it() } }
+    }
+}
+
+/**
+ * Create a new coroutine running the function while the predicate is met
+ *
+ * @param sensor data source for predicate
+ * @param predicate function to check if the coroutine should still be running
+ * @param block function to run
+ * @return coroutine which runs until `predicate` returns false
+ */
+fun CoroutineScope.runWhile(predicate: () -> Boolean, block: Block) = startRoutine("runWhile") {
+    choreography {
+        if (predicate()) {
+            val job = launch { block() }
+
+            var runOnTick: Cancel? = null
+            runOnTick = EventLoop.runOnTick {
+                if (!predicate()) {
+                    runOnTick?.cancel()
+                    job.cancel()
+                }
+            }
+
+            job.join()
+        }
+    }
+}
+
+/**
+ * Pauses the coroutine for some time
+ *
+ * @param time period to delay for
+ */
+suspend fun delay(time: Time) =
+        delay(time.milli(Second).toLong())
+
+/**
+ * Cancels the given function if it takes too long
+ *
+ * @param time maximum time to run the function for
+ * @param block function to run
+ */
+suspend fun withTimeout(time: Time, block: Block) =
+        withTimeoutOrNull(time.milli(Second).toLong(), block)
