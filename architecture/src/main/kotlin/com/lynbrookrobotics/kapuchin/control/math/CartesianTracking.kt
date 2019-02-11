a467f3a7cfd0d74dba170259db6173c03ca4a839
Andy Min <andrewkmin@gmail.com>
2019-02-10 21:34:16 -0800

    Rotation Matrix Tracking (#61)
    * Create and implement RotationMatrixTracking
    
    * Bug fixes
    
    * Fixed but needs more testing
    
    * Move SensorScope to routines package
    
    * Separate out free and bound sensor update strategies
    
    * Wow it worksgit add -Agit add -A
    
    * Implemented cache but needs testing
    
    * Write choreography function
    
    * Rename Orchestration tests to Choreography tests
    
    * Refactor routine orchestration functions to choreography arch
    
    * Move choreo functions from Routine.kt to Choreo....kt
    
    * Code cleanup
    
    * Get it to work
    
    * Fix prints in choreo functions
    
    * Added unit testing
    
    * Reduced object creation for performance
    
    * Fix Warmup.kt
    
    * Integrate choreo arch with 2019 robot
    
    * Fix teleop jitters
    
    * Fix teleop more jitters

--- architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/control/math/CartesianTracking.kt
+++ architecture/src/main/kotlin/com/lynbrookrobotics/kapuchin/control/math/CartesianTracking.kt
@@ -4,8 +4,8 @@ import com.lynbrookrobotics.kapuchin.control.data.*
 import info.kunalsheth.units.generated.*
 import info.kunalsheth.units.math.*
 
-private fun theta(sl: Length, sr: Length, track: Length) = (sl - sr) / track * Radian
-private fun s(sl: Length, sr: Length) = avg(sl, sr)
+fun theta(sl: Length, sr: Length, track: Length) = (sl - sr) / track * Radian
+fun s(sl: Length, sr: Length) = avg(sl, sr)
 
 // todo: unit test!
 fun simpleVectorTracking(
@@ -21,13 +21,71 @@ fun simpleVectorTracking(
         val theta = theta(sl, sr, trackLength)
 
         pos += Position(
-            //Using compass bearings not trig bearings
+                //Using compass bearings not trig bearings
                 x = s * sin(pos.bearing),
                 y = s * cos(pos.bearing),
                 bearing = theta
         )
 
         return pos
+    }
+}
+
+class RotationMatrixTracking(
+        private var trackLength: Length, init: Position, private var cache: Map<Angle, RotationMatrix> = emptyMap()
+) : (Length, Length) -> Unit {
+
+    private var lpx: Length
+    private var lpy: Length
+    private var rpx: Length
+    private var rpy: Length
+
+    init {
+        UomVector(-trackLength / 2, 0.Foot).let {
+            (RotationMatrix(init.bearing) rz it) + init.vector
+        }.let { (lpx, lpy) ->
+            this.lpx = lpx
+            this.lpy = lpy
+        }
+
+        UomVector(trackLength / 2, 0.Foot).let {
+            (RotationMatrix(init.bearing) rz it) + init.vector
+        }.let { (rpx, rpy) ->
+            this.rpx = rpx
+            this.rpy = rpy
+        }
+    }
+
+    var x = init.x
+    var y = init.y
+    var bearing = init.bearing
+
+    override fun invoke(sl: Length, sr: Length) {
+        val tl = theta(sl, 0.Foot, trackLength)
+        val tr = theta(0.Foot, sr, trackLength)
+
+        val ml = cache[tl] ?: RotationMatrix(tl)//.also { println("Cache miss L") }
+        val mr = cache[tr] ?: RotationMatrix(tr)//.also { println("Cache miss R") }
+
+        // copy of previous state
+        val olpx = lpx
+        val olpy = lpy
+        val orpx = rpx
+        val orpy = rpy
+
+        // do matrix operations without creating new objects
+        val olpxMorpx = olpx - orpx
+        val olpyMorpy = olpy - orpy
+
+        lpx = ml.rzComponentX(olpxMorpx, olpyMorpy) + orpx
+        lpy = ml.rzComponentY(olpxMorpx, olpyMorpy) + orpy
+
+        rpx = mr.rzComponentX(-olpxMorpx, -olpyMorpy) + olpx
+        rpy = mr.rzComponentY(-olpxMorpx, -olpyMorpy) + olpy
+
+        x = avg(lpx, rpx)
+        y = avg(lpy, lpy)
+        bearing += theta(sl, sr, trackLength)
     }
 }
 
