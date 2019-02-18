d101002ba03bd3c2eafe8bffd99f31c3a5391ae6
Kunal Sheth <kunal@kunalsheth.info>
2019-02-18 04:54:19 -0800

    Fix CollectorSlider

--- nineteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/intake/collector/CollectorSlider.kt
+++ nineteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/intake/collector/CollectorSlider.kt
@@ -9,27 +9,18 @@ import com.lynbrookrobotics.kapuchin.timing.*
 import com.lynbrookrobotics.kapuchin.timing.clock.*
 import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMaxLowLevel.MotorType
-import edu.wpi.first.wpilibj.Encoder
 import info.kunalsheth.units.generated.*
 import info.kunalsheth.units.math.*
 
 class CollectorSliderComponent(hardware: CollectorSliderHardware) : Component<CollectorSliderComponent, CollectorSliderHardware, DutyCycle>(hardware, EventLoop) {
 
-    //positive is to the robot's right
-    //negative is to the robot's left
+    val kP by pref(3, Volt, 1, Inch)
 
-    val defaultPosition by pref(0, Inch)
-
-    val kP = 10.Percent / 2.Inch
-
-    override val fallbackController: CollectorSliderComponent.(Time) -> DutyCycle = {
-        kP * (defaultPosition - hardware.position.optimizedRead(it, 5.milli(Second)).y)
-    }
+    override val fallbackController: CollectorSliderComponent.(Time) -> DutyCycle = { 0.Percent }
 
     override fun CollectorSliderHardware.output(value: DutyCycle) {
         hardware.esc.set(value.Each)
     }
-
 }
 
 class CollectorSliderHardware : SubsystemHardware<CollectorSliderHardware, CollectorSliderComponent>() {
@@ -38,27 +29,28 @@ class CollectorSliderHardware : SubsystemHardware<CollectorSliderHardware, Colle
     override val syncThreshold: Time = 5.milli(Second)
     override val name: String = "Collector Slider"
 
-    val operatingVoltage by pref(12, Volt)
-    val currentLimit by pref(30, Ampere)
-    val startupFrictionCompensation by pref(1.4, Volt)
-
-    val range by pref(3)
+    val operatingVoltage by pref(11, Volt)
+    val currentLimit by pref(20, Ampere)
+    val startupFrictionCompensation by pref(0.5, Volt)
 
     val escCanId by pref(20)
-    val esc by hardw { CANSparkMax(escCanId, MotorType.kBrushless) }
-
-    val chA by pref(10)
-    val chB by pref(11)
-    val resolution by pref(42)
-
-    val distance by pref(16, Inch)
-    val perDegree by pref(1080, Degree)
-
-    val encoder = Encoder(chA, chB)
-
-    val position = sensor {
-        distance * (EncoderConversion(resolution, 360.Degree).angle(encoder.raw.toDouble()) / perDegree) stampWith it
+    val esc by hardw { CANSparkMax(escCanId, MotorType.kBrushless) }.configure {
+        it.setSmartCurrentLimit(currentLimit.Ampere.toInt())
     }
+    val encoder by hardw { esc.encoder }
+
+    val conversion by pref {
+        val encoderRotations by pref(10, Turn)
+        val perSliderDistance by pref(1, Inch)
+
+        val resolution by pref(42)
+        ({
+            val enc = EncoderConversion(resolution, 1.Turn)
+            fun(ticks: Double) = enc.angle(ticks) / encoderRotations * perSliderDistance
+        })
+    }
+
+    val position = sensor(encoder) { conversion(position) stampWith it }
 
     init {
         EventLoop.runOnTick { position.optimizedRead(it, syncThreshold) }
