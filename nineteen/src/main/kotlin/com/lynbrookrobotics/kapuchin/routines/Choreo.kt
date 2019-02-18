5ac30a15ae530d77aa6f79518f26ce87fbac9629
Kunal Sheth <kunal@kunalsheth.info>
2019-02-18 03:53:33 -0800

    Add more of Andrew's pseudocode

--- nineteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/routines/Choreo.kt
+++ nineteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/routines/Choreo.kt
@@ -20,21 +20,37 @@ suspend fun collectWallPanel() = startChoreo("Collect Wall Panel") {
 }
 
 suspend fun collectGroundPanel() = startChoreo("Collect Ground Panel") {
+    //elevator handoff pos
+//IH_pivot *down* (not coll) pos
+//_wait for driver input_
+//IH_velcroSol out (and stay out)
+//IH_hookSol down
+//IH_pivot *handoffpos*
     TODO()
 }
 
 suspend fun deployCargo() = startChoreo("Deploy Cargo") {
+    //elevator to scoring pos (seperately input by driver)
+//_auto find target_
+//IC_rollerB and IC_rollerT out
     TODO()
 }
 
 suspend fun deployPanel() = startChoreo("Deploy Panel") {
+    //_slider / drivetrain auto align_
+//IC_hookSliderSol out
+//_driver input, prob letting go of a button they hold to start this action_
+//IC_hookSol down
+//IC_hookSliderSol in
     TODO()
 }
 
 suspend fun pushPanel() = startChoreo("Push Panel") {
+//idk what 6 is,
     TODO()
 }
 
 suspend fun unleashTheCobra() = startChoreo("Unleash the Cobra") {
+//just goo 2 motors until some sort of sensor
     TODO()
 }
