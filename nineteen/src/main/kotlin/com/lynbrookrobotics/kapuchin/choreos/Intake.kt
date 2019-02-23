a4fe31c7f9d01fa76c0a035eea2973d9ea88faf6
Andy Min <andrewkmin@gmail.com>
2019-02-23 14:00:56 -0800

    HookSliderPosition -> HookSliderState

--- nineteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/choreos/Intake.kt
+++ nineteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/choreos/Intake.kt
@@ -8,7 +8,7 @@ import com.lynbrookrobotics.kapuchin.subsystems.intake.collector.*
 import com.lynbrookrobotics.kapuchin.subsystems.intake.collector.hookslider.*
 import com.lynbrookrobotics.kapuchin.subsystems.intake.collector.pivot.*
 import com.lynbrookrobotics.kapuchin.subsystems.intake.collector.HookPosition.*
-import com.lynbrookrobotics.kapuchin.subsystems.intake.collector.hookslider.HookSliderPosition.*
+import com.lynbrookrobotics.kapuchin.subsystems.intake.collector.hookslider.HookSliderState.*
 import com.lynbrookrobotics.kapuchin.subsystems.intake.collector.slider.*
 import com.lynbrookrobotics.kapuchin.subsystems.intake.handoff.*
 import com.lynbrookrobotics.kapuchin.subsystems.intake.handoff.pivot.*
