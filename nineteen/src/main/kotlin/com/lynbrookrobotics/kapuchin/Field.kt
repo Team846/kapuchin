923d40af3e02acb9cc3229d17ee7e956fe33b96f
Alvyn Wang <alvyn.wang@gmail.com>
2019-03-16 18:12:15 -0700

    Change to vertical coordinate system and add choreos to go to various locations on the field

--- nineteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/Field.kt
+++ nineteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/Field.kt
@@ -6,49 +6,49 @@ import info.kunalsheth.units.generated.*
 //Based on the CAD diagram of the field.
 
 //HAB
-val leftHAB = UomVector(-259.125.Inch, 42.66666667.Inch)
-val centerHAB = UomVector(-259.125.Inch, 0.Inch)
-val rightHAB = UomVector(-259.125.Inch, -42.66666667.Inch)
+val leftHAB = UomVector(-21.33333.Inch, 82.875.Inch)
+val centerHAB = UomVector(0.Inch, 82.85.Inch)
+val rightHAB = UomVector(21.33333.Inch, 82.85.Inch)
 
 //Cargo
-val cargoXSeparation = 21.75.Inch
-val farCargoX = -64.375.Inch
-val middleCargoX = farCargoX + cargoXSeparation
-val closeCargoX = farCargoX + cargoXSeparation * 2.0
-val cargoY = 45.875.Inch
+val cargoYSeparation = 21.75.Inch
+val closeCargoY = 259.625.Inch
+val middleCargoY = closeCargoY + cargoYSeparation
+val farCargoY = closeCargoY + cargoYSeparation * 2.0
+val cargoX = 45.875.Inch 
 
-val leftCloseCargo = UomVector(farCargoX, cargoY)
-val leftMiddleCargo = UomVector(middleCargoX, cargoY)
-val leftFarCargo = UomVector(closeCargoX, cargoY)
+val leftCloseCargo = UomVector(cargoX, closeCargoY)
+val leftMiddleCargo = UomVector(cargoX, middleCargoY)
+val leftFarCargo = UomVector(cargoX, farCargoY)
 
-val rightCloseCargo = UomVector(farCargoX, -cargoY)
-val rightMiddleCargo = UomVector(middleCargoX, -cargoY)
-val rightFarCargo = UomVector(closeCargoX, -cargoY)
+val rightCloseCargo = UomVector(-cargoX, closeCargoY)
+val rightMiddleCargo = UomVector(-cargoX, middleCargoY)
+val rightFarCargo = UomVector(-cargoX, farCargoY)
 
-val leftFrontCargo = UomVector(-122.625.Inch, 10.875.Inch)
-val rightFrontCargo = UomVector(-122.625.Inch, 10.875.Inch)
+val leftFrontCargo = UomVector(-10.875.Inch, 201.375.Inch)
+val rightFrontCargo = UomVector(10.875.Inch, 201.375.Inch)
 
 //Rocket
-val closeRocketX = -126.625.Inch
-val closeRocketY = 134.222.Inch
+val closeRocketX = 134.222.Inch
+val closeRocketY = 197.58.Inch
 
-val middleRocketX = -96.Inch
-val middleRocketY = 115.558.Inch
+val middleRocketX = 115.558.Inch
+val middleRocketY = 228.Inch
 
-val farRocketX = -65.587.Inch
-val farRocketY = 134.222.Inch
+val farRocketX = 134.222.Inch
+val farRocketY = 258.413.Inch
 
-val leftCloseRocket = UomVector(closeRocketX, closeRocketY)
-val leftMiddleRocket = UomVector(middleRocketX, middleRocketY)
-val leftFarRocket = UomVector(farRocketX, farRocketY)
+val leftCloseRocket = UomVector(-closeRocketX, closeRocketY)
+val leftMiddleRocket = UomVector(-middleRocketX, middleRocketY)
+val leftFarRocket = UomVector(-farRocketX, farRocketY)
 
-val rightCloseRocket = UomVector(closeRocketX, -closeRocketY)
-val rightMiddleRocket = UomVector(middleRocketX, -middleRocketY)
-val rightFarRocket = UomVector(farRocketX, -farRocketY)
+val rightCloseRocket = UomVector(closeRocketX, closeRocketY)
+val rightMiddleRocket = UomVector(middleRocketX, middleRocketY)
+val rightFarRocket = UomVector(farRocketX, farRocketY)
 
 //Loading station
-val leftLoadingStation = UomVector(-307.313.Inch, 135.285.Inch)
-val rightLoadingStation = UomVector(-307.313.Inch, -135.285.Inch)
+val leftLoadingStation = UomVector(-135.285.Inch, 16.687.Inch)
+val rightLoadingStation = UomVector(135.285.Inch, 16.687.Inch)
 
 
 
--- nineteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/choreos/Drivetrain.kt
+++ nineteen/src/main/kotlin/com/lynbrookrobotics/kapuchin/choreos/Drivetrain.kt
@@ -16,6 +16,9 @@ suspend fun Subsystems.drivetrainTeleop() = startChoreo("Drivetrain teleop") {
     val goToRightLoadingStation by driver.goToRightLoadingStation.readEagerly().withoutStamps
 
 
+    //TODO: goToLeftFrontCargo
+    //TODO: goToRightFrontCargo
+
     val goToLeftCloseCargo by driver.goToLeftCloseCargo.readEagerly().withoutStamps
     val goToRightCloseCargo by driver.goToRightCloseCargo.readEagerly().withoutStamps
 
@@ -25,9 +28,11 @@ suspend fun Subsystems.drivetrainTeleop() = startChoreo("Drivetrain teleop") {
     val goToLeftFarCargo by driver.goToLeftFarCargo.readEagerly().withoutStamps
     val goToRightFarCargo by driver.goToRightFarCargo.readEagerly().withoutStamps
 
-
     val goToLeftCloseRocket by driver.goToLeftCloseRocket.readEagerly().withoutStamps
     val goToRightCloseRocket by driver.goToRightCloseRocket.readEagerly().withoutStamps
+
+    //TODO: goToLeftMiddleRocket
+    //TODO: goToRightMiddleRocket
 
     val goToLeftFarRocket by driver.goToLeftFarRocket.readEagerly().withoutStamps
     val goToRightFarRocket by driver.goToRightFarRocket.readEagerly().withoutStamps
@@ -39,6 +44,78 @@ suspend fun Subsystems.drivetrainTeleop() = startChoreo("Drivetrain teleop") {
                         0.5.FootPerSecondSquared,
                         3.FootPerSecond
                 ), leftLoadingStation, 0.5.Inch)
+            }
+            runWhile({goToRightLoadingStation}) {
+                drivetrain.waypoint(trapezoidalMotionProfile(
+                        0.5.FootPerSecondSquared,
+                        3.FootPerSecond
+                ), rightLoadingStation, 0.5.Inch)
+            }
+            runWhile({goToRightCloseCargo}) {
+                drivetrain.waypoint(trapezoidalMotionProfile(
+                        0.5.FootPerSecondSquared,
+                        3.FootPerSecond
+                ), rightCloseCargo, 0.5.Inch)
+            }
+            runWhile({goToLeftCloseCargo}) {
+                drivetrain.waypoint(trapezoidalMotionProfile(
+                        0.5.FootPerSecondSquared,
+                        3.FootPerSecond
+                ), leftCloseCargo, 0.5.Inch)
+            }
+            runWhile({goToRightCloseCargo}) {
+                drivetrain.waypoint(trapezoidalMotionProfile(
+                        0.5.FootPerSecondSquared,
+                        3.FootPerSecond
+                ), rightCloseCargo, 0.5.Inch)
+            }
+            runWhile({goToLeftMiddleCargo}) {
+                drivetrain.waypoint(trapezoidalMotionProfile(
+                        0.5.FootPerSecondSquared,
+                        3.FootPerSecond
+                ), leftMiddleCargo, 0.5.Inch)
+            }
+            runWhile({goToRightMiddleCargo}) {
+                drivetrain.waypoint(trapezoidalMotionProfile(
+                        0.5.FootPerSecondSquared,
+                        3.FootPerSecond
+                ), rightMiddleCargo, 0.5.Inch)
+            }
+            runWhile({goToLeftFarCargo}) {
+                drivetrain.waypoint(trapezoidalMotionProfile(
+                        0.5.FootPerSecondSquared,
+                        3.FootPerSecond
+                ), rightFarCargo, 0.5.Inch)
+            }
+            runWhile({goToRightFarCargo}) {
+                drivetrain.waypoint(trapezoidalMotionProfile(
+                        0.5.FootPerSecondSquared,
+                        3.FootPerSecond
+                ), rightFarCargo, 0.5.Inch)
+            }
+            runWhile({goToLeftCloseRocket}) {
+                drivetrain.waypoint(trapezoidalMotionProfile(
+                        0.5.FootPerSecondSquared,
+                        3.FootPerSecond
+                ), leftCloseRocket, 0.5.Inch)
+            }
+            runWhile({goToRightCloseRocket}) {
+                drivetrain.waypoint(trapezoidalMotionProfile(
+                        0.5.FootPerSecondSquared,
+                        3.FootPerSecond
+                ), rightCloseRocket, 0.5.Inch)
+            }
+            runWhile({goToLeftFarRocket}) {
+                drivetrain.waypoint(trapezoidalMotionProfile(
+                        0.5.FootPerSecondSquared,
+                        3.FootPerSecond
+                ), leftFarRocket, 0.5.Inch)
+            }
+            runWhile({goToRightFarRocket}) {
+                drivetrain.waypoint(trapezoidalMotionProfile(
+                        0.5.FootPerSecondSquared,
+                        3.FootPerSecond
+                ), rightFarRocket, 0.5.Inch)
             }
             runWhile({ !visionAlign }) {
                 drivetrain.teleop(driver)
