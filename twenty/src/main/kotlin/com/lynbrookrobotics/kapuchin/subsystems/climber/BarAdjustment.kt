96806eb7f9e584096e9ba49344706ce6ab49ac16
Ankith Madadi <ankithm28@gmail.com>
2020-01-18 15:23:42 -0800

    subsystems reformatting

---
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/climber/BarAdjustment.kt
@@ -0,0 +1,43 @@
+package com.lynbrookrobotics.kapuchin.subsystems.climber
+
+import com.lynbrookrobotics.kapuchin.hardware.*
+import com.lynbrookrobotics.kapuchin.preferences.*
+import com.lynbrookrobotics.kapuchin.subsystems.*
+import com.lynbrookrobotics.kapuchin.timing.*
+import com.revrobotics.CANSparkMax
+import com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushed
+import info.kunalsheth.units.generated.*
+
+class BarAdjustmentComponent (hardware: BarAdjustmentHardware) : Component<BarAdjustmentComponent, BarAdjustmentHardware, DutyCycle>(hardware) {
+    override val fallbackController: BarAdjustmentComponent.(Time) -> DutyCycle ={0.Percent}
+
+    override fun BarAdjustmentHardware.output(value: DutyCycle) {
+        baradjustmentEsc.set(value.Each)
+
+
+
+
+    }
+
+
+
+}
+class BarAdjustmentHardware : SubsystemHardware<BarAdjustmentHardware, BarAdjustmentComponent>() {
+    override val period: Time = 30.Millisecond
+    override val syncThreshold: Time = 30.Millisecond
+    override val priority: Priority = Priority.Low
+    override val name: String = "Bar Adjustment"
+
+    val baradjustmentEscId by pref(10)
+    val baradjustmentEscInversion by pref(false)
+
+    val escConfig by escConfigPref(
+            defaultNominalOutput = 0.5.Volt,
+            defaultContinuousCurrentLimit = 25.Ampere,
+            defaultPeakCurrentLimit = 35.Ampere
+    )
+
+    val baradjustmentEsc by hardw { CANSparkMax(baradjustmentEscId, kBrushed) }.configure {
+
+    }
+}
---
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/climber/ClimberStow.kt
@@ -0,0 +1,41 @@
+package com.lynbrookrobotics.kapuchin.subsystems.climber
+
+
+
+import com.lynbrookrobotics.kapuchin.*
+import com.lynbrookrobotics.kapuchin.hardware.*
+import com.lynbrookrobotics.kapuchin.subsystems.*
+import com.lynbrookrobotics.kapuchin.subsystems.climber.ClimberStowComponent.*
+import com.lynbrookrobotics.kapuchin.subsystems.climber.ClimberStowState.*
+import com.lynbrookrobotics.kapuchin.timing.*
+import edu.wpi.first.wpilibj.Solenoid
+import info.kunalsheth.units.generated.*
+import info.kunalsheth.units.math.*
+
+
+class ClimberStowComponent(hardware: ClimberStowHardware) : Component<ClimberStowComponent, ClimberStowHardware, ClimberStowState>(hardware, Subsystems.pneumaticTicker) {
+
+    override val fallbackController: ClimberStowComponent.(Time) -> ClimberStowState = { Up }
+
+    override fun ClimberStowHardware.output(value: ClimberStowState) {
+
+        solenoid.set(value.output)
+
+    }
+
+
+    class ClimberStowHardware : SubsystemHardware<ClimberStowHardware, ClimberStowComponent>() {
+        override val priority: Priority = Priority.Low
+        override val period: Time = 100.milli(Second)
+        override val syncThreshold: Time = 20.milli(Second)
+        override val name: String = "Climber Stow"
+
+        
+        val solenoidPort = 0
+        val solenoid by hardw { Solenoid(solenoidPort) }
+
+
+    }
+}
+
+
