b50cd8b3f95e7acca26f0e77971dbc300d30597b
Aanand Kainth <aanand@akainth.me>
2020-01-27 23:48:27 -0800

    Optimize imports in Intake.kt

--- twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/choreos/Intake.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/choreos/Intake.kt
@@ -1,14 +1,7 @@
 package com.lynbrookrobotics.kapuchin.choreos
 
 import com.lynbrookrobotics.kapuchin.*
-import com.lynbrookrobotics.kapuchin.control.data.*
 import com.lynbrookrobotics.kapuchin.routines.*
-import com.lynbrookrobotics.kapuchin.subsystems.collector.*
-import com.lynbrookrobotics.kapuchin.subsystems.collector.hookslider.*
-import com.lynbrookrobotics.kapuchin.subsystems.collector.pivot.*
-import com.lynbrookrobotics.kapuchin.timing.*
-import info.kunalsheth.units.generated.*
-import kotlinx.coroutines.*
 
 suspend fun Subsystems.intakeTeleop() = startChoreo("Intake teleop") {
     choreography {
