1f8fc40b2a1ab543da1f6966f0379b40808ca812
Aanand Kainth <aanand@akainth.me>
2020-01-27 17:23:23 -0800

    Stub 2020 auto code

--- twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/Auto.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/Auto.kt
@@ -1,78 +1,33 @@
 package com.lynbrookrobotics.kapuchin
 
-import com.lynbrookrobotics.kapuchin.choreos.*
 import com.lynbrookrobotics.kapuchin.control.data.*
 import com.lynbrookrobotics.kapuchin.control.math.kinematics.*
 import com.lynbrookrobotics.kapuchin.logging.*
 import com.lynbrookrobotics.kapuchin.logging.Level.*
 import com.lynbrookrobotics.kapuchin.routines.*
 import info.kunalsheth.units.generated.*
-import kotlinx.coroutines.launch
 
-suspend fun Subsystems.cargoShipSandstorm() = startChoreo("Rocket Sandstorm") {
-    val habToCloseCargo = loadTrajectory("hab_to_close_cargo.tsv", 60.Percent)
-    val closeCargoToLoading = loadTrajectory("close_cargo_to_loading.tsv", 60.Percent)
-    val loadingToMiddleCargo = loadTrajectory("loading_to_middle_cargo.tsv", 60.Percent)
+suspend fun Subsystems.rightShoot3Collect3Shoot3() = startChoreo("Right Auto") {
 
     val interrupt by driver.interruptAuto.readEagerly(0.Second).withoutStamps
-
-    val currentPosition by drivetrain.hardware.position.readEagerly(0.Second).withoutStamps
-    val linePosition by lineScanner.linePosition.readEagerly(0.Second).withoutStamps
 
     System.gc()
 
     choreography {
         runWhile({ !interrupt }) {
-            launch { lift?.set(lift.panelLowRocket) }
-            drivetrain.followTrajectory(20.Inch, 8.Inch, 5.FootPerSecondSquared, habToCloseCargo)
-            launch { collectorSlider?.trackLine(lineScanner, electrical) }
-            launch { drivetrain.openLoop(30.Percent) }
-            lift?.set(lift.cargoCargoShip)
-            deployCargo(true)
-            lift?.set(lift.panelCollect)
+            freeze()
+        }
+        log(Debug) { "Interrupted" }
+    }
+}
+suspend fun Subsystems.leftCollect2Shoot5() = startChoreo("Left Auto") {
 
-            val origin1 = currentPosition.vector +
-                    (RotationMatrix(currentPosition.bearing) rz UomVector(
-                            x = -(linePosition ?: 0.Inch),
-                            y = 0.Inch
-                    ))
-            val bearing1 = currentPosition.bearing
+    val interrupt by driver.interruptAuto.readEagerly(0.Second).withoutStamps
 
-            withTimeout(0.5.Second) { drivetrain.openLoop(-30.Percent) }
+    System.gc()
 
-            drivetrain.followTrajectory(20.Inch, 8.Inch, 5.FootPerSecondSquared, closeCargoToLoading,
-                    Position(origin1.x, origin1.y, bearing1)
-            )
-//            launch { trackLine() }
-//            launch { drivetrain.openLoop(30.Percent) }
-//            lift?.set(lift.panelMidRocket)
-//            deployPanel()
-//            withTimeout(0.5.Second) { drivetrain.openLoop(-30.Percent) }
-//
-//            val origin2 = currentPosition.vector +
-//                    (RotationMatrix(currentPosition.bearing) rz UomVector(
-//                            x = -(linePosition ?: 0.Inch),
-//                            y = 0.Inch
-//                    ))
-//
-//            drivetrain.followTrajectory(20.Inch, 2.Inch, 5.FootPerSecondSquared, loadingToMiddleCargo,
-//                    Position(origin2.x, origin2.y, currentPosition.bearing)
-//            )
-
-//            withTimeout(2.Second) {
-//                launch { trackLine() }
-//                lift?.set(lift.panelHighRocket)
-//            }
-//            withTimeout(1.Second) { deployPanel() }
-
-//            val mtrx = RotationMatrix(startingPosition.bearing)
-//            val newLocation = startingPosition.vector + (mtrx rz UomVector(linePosition ?: 0.Foot, 0.Foot, 0.Foot))
-//            val newOrigin = Position(newLocation.x, newLocation.y, startingPosition.bearing)
-
-//            withTimeout(1.Second) { drivetrain.openLoop(-50.Percent) }
-//
-//            drivetrain.followTrajectory(20.Inch, 4.Inch, 5.FootPerSecondSquared, rocketToLoading, newOrigin)
-
+    choreography {
+        runWhile({ !interrupt }) {
             freeze()
         }
         log(Debug) { "Interrupted" }
@@ -82,7 +37,7 @@ suspend fun Subsystems.cargoShipSandstorm() = startChoreo("Rocket Sandstorm") {
 fun Subsystems.loadTrajectory(name: String, performance: Dimensionless): List<TimeStamped<Waypt>> {
     val waypts = Thread.currentThread()
             .contextClassLoader
-            .getResourceAsStream("com/lynbrookrobotics/kapuchin/paths/$name")
+            .getResourceAsStream("com/lynbrookrobotics/kapuchin/paths/$name")!!
             .bufferedReader()
             .lineSequence()
             .drop(1)
