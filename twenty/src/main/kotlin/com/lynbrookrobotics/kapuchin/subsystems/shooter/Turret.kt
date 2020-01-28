985c5f120b7d7a77582ce9813e73df695309c89b
Aanand Kainth <aanand@akainth.me>
2020-01-28 07:31:33 -0800

    Make the default limit switch polarity configurable

--- twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/shooter/Turret.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/shooter/Turret.kt
@@ -8,6 +8,8 @@ import com.lynbrookrobotics.kapuchin.subsystems.*
 import com.lynbrookrobotics.kapuchin.timing.*
 import com.lynbrookrobotics.kapuchin.timing.Priority.*
 import com.revrobotics.CANDigitalInput
+import com.revrobotics.CANDigitalInput.LimitSwitchPolarity.kNormallyClosed
+import com.revrobotics.CANDigitalInput.LimitSwitchPolarity.kNormallyOpen
 import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMaxLowLevel
 import info.kunalsheth.units.generated.*
@@ -15,7 +17,8 @@ import info.kunalsheth.units.generated.*
 
 class TurretComponent(hardware: TurretHardware) : Component<TurretComponent, TurretHardware, OffloadedOutput>(hardware) {
     override val fallbackController: TurretComponent.(Time) -> OffloadedOutput = {
-        PercentOutput(hardware.escConfig, 0.Percent) }
+        PercentOutput(hardware.escConfig, 0.Percent)
+    }
 
 
     override fun TurretHardware.output(value: OffloadedOutput) {
@@ -29,14 +32,10 @@ class TurretComponent(hardware: TurretHardware) : Component<TurretComponent, Tur
 }
 
 class TurretHardware : SubsystemHardware<TurretHardware, TurretComponent>() {
-    override val period: Time
-        get() = 50.Millisecond
-    override val syncThreshold: Time
-        get() = 20.Millisecond
-    override val priority: Priority
-        get() = High
-    override val name: String
-        get() = "Turret"
+    override val period: Time = 50.Millisecond
+    override val syncThreshold: Time = 20.Millisecond
+    override val priority: Priority = High
+    override val name: String = "Turret"
 
     private val turretEscId by pref(16)
 
@@ -49,10 +48,17 @@ class TurretHardware : SubsystemHardware<TurretHardware, TurretComponent>() {
 
     val turretEsc by hardw { CANSparkMax(turretEscId, CANSparkMaxLowLevel.MotorType.kBrushless) }
 
-    val leftStop: CANDigitalInput by hardw { turretEsc.getForwardLimitSwitch(CANDigitalInput.LimitSwitchPolarity.kNormallyOpen) }.configure {
+    private val switchNormallyOpen by pref(true)
+
+    private val limitSwitchPolarity = if (switchNormallyOpen) {
+        kNormallyOpen
+    } else {
+        kNormallyClosed
+    }
+    val leftStop: CANDigitalInput by hardw { turretEsc.getForwardLimitSwitch(limitSwitchPolarity) }.configure {
         +it.enableLimitSwitch(true)
     }
-    val rightStop: CANDigitalInput by hardw { turretEsc.getReverseLimitSwitch(CANDigitalInput.LimitSwitchPolarity.kNormallyOpen) }.configure {
+    val rightStop: CANDigitalInput by hardw { turretEsc.getReverseLimitSwitch(limitSwitchPolarity) }.configure {
         +it.enableLimitSwitch(true)
     }
 
