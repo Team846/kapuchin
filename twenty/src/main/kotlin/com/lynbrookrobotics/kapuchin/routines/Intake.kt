097e413984039cda1be7b25ced9302b6f85571e8
Aanand Kainth <aanand@akainth.me>
2020-01-27 23:50:29 -0800

    Update Intake routine code

--- twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/routines/Intake.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/routines/Intake.kt
@@ -1,33 +1,19 @@
 package com.lynbrookrobotics.kapuchin.routines
 
-import com.lynbrookrobotics.kapuchin.control.data.*
-import com.lynbrookrobotics.kapuchin.control.electrical.*
-import com.lynbrookrobotics.kapuchin.control.math.*
-import com.lynbrookrobotics.kapuchin.subsystems.*
+import com.lynbrookrobotics.kapuchin.hardware.offloaded.*
 import com.lynbrookrobotics.kapuchin.subsystems.collector.*
-import com.lynbrookrobotics.kapuchin.subsystems.collector.hookslider.*
-import com.lynbrookrobotics.kapuchin.subsystems.collector.pivot.*
-import com.lynbrookrobotics.kapuchin.subsystems.collector.slider.*
-import com.lynbrookrobotics.kapuchin.subsystems.driver.*
-import com.lynbrookrobotics.kapuchin.subsystems.drivetrain.*
 import info.kunalsheth.units.generated.*
-import info.kunalsheth.units.math.*
 
-suspend fun CollectorPivotComponent.set(target: CollectorPivotState) = startRoutine("Set") {
+suspend fun IntakePivotComponent.set(target: IntakePivotState) = startRoutine("Set") {
     controller { target }
 }
 
-suspend fun CollectorRollersComponent.spin(electrical: ElectricalSystemHardware, top: V, bottom: V = top) = startRoutine("Spin") {
-    val vBat by electrical.batteryVoltage.readEagerly.withoutStamps
-
+suspend fun CollectorRollersComponent.spin(speed: ElectricCurrent) = startRoutine("Spin") {
     controller {
-        TwoSided(
-                voltageToDutyCycle(top, vBat),
-                voltageToDutyCycle(bottom, vBat)
-        )
+        CurrentOutput(hardware.escConfig, speed)
     }
 }
 
-suspend fun CollectorRollersComponent.set(state: TwoSided<DutyCycle>) = startRoutine("Set") {
+suspend fun CollectorRollersComponent.set(state: OffloadedOutput) = startRoutine("Set") {
     controller { state }
 }
