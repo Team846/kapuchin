f3bfd15091ce8e4aea82aedffeb2b3fdd66f423c
Aanand Kainth <aanand@akainth.me>
2020-01-27 21:40:51 -0800

    Clean the collector rollers component

--- twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/collector/CollectorRollers.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/collector/CollectorRollers.kt
@@ -1,9 +1,5 @@
 package com.lynbrookrobotics.kapuchin.subsystems.collector
 
-import com.ctre.phoenix.motorcontrol.ControlMode
-import com.ctre.phoenix.motorcontrol.can.VictorSPX
-import com.lynbrookrobotics.kapuchin.*
-import com.lynbrookrobotics.kapuchin.control.data.*
 import com.lynbrookrobotics.kapuchin.hardware.*
 import com.lynbrookrobotics.kapuchin.hardware.offloaded.*
 import com.lynbrookrobotics.kapuchin.preferences.*
@@ -15,13 +11,12 @@ import info.kunalsheth.units.generated.*
 import info.kunalsheth.units.math.*
 
 class CollectorRollersComponent(hardware: CollectorRollersHardware) : Component<CollectorRollersComponent, CollectorRollersHardware, OffloadedOutput>(hardware) {
-
     val cargoCollectSpeed by pref(11, Volt)
     val cargoReleaseSpeed by pref(-6, Volt)
 
-
     override val fallbackController: CollectorRollersComponent.(Time) -> OffloadedOutput = {
-        PercentOutput(hardware.escConfig, 0.Percent) }
+        PercentOutput(hardware.escConfig, 0.Percent)
+    }
 
     override fun CollectorRollersHardware.output(value: OffloadedOutput) {
         value.writeTo(rollersEsc)
@@ -34,9 +29,8 @@ class CollectorRollersHardware : SubsystemHardware<CollectorRollersHardware, Col
     override val syncThreshold: Time = 20.milli(Second)
     override val name: String = "Collector Rollers"
 
-    private val invert by pref(false)
+    private val invertRollers by pref(false)
 
-    val rollersEscId by pref(0)
     val escConfig by escConfigPref(
             defaultNominalOutput = 0.5.Volt,
 
@@ -44,6 +38,6 @@ class CollectorRollersHardware : SubsystemHardware<CollectorRollersHardware, Col
             defaultPeakCurrentLimit = 35.Ampere
     )
 
+    private val rollersEscId by pref(0)
     val rollersEsc by hardw { CANSparkMax(rollersEscId, kBrushless) }
-
 }
