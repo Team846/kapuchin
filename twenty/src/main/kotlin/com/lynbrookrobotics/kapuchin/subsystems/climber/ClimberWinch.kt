f1dc0cfaf4e2dd5979506db81c4260765b547072
Aanand Kainth <aanand@akainth.me>
2020-01-28 16:30:56 -0800

    Add 2020 climber subsystems

---
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/climber/ClimberWinch.kt
@@ -0,0 +1,33 @@
+package com.lynbrookrobotics.kapuchin.subsystems.climber
+
+import com.lynbrookrobotics.kapuchin.hardware.*
+import com.lynbrookrobotics.kapuchin.hardware.offloaded.*
+import com.lynbrookrobotics.kapuchin.preferences.*
+import com.lynbrookrobotics.kapuchin.subsystems.*
+import com.lynbrookrobotics.kapuchin.timing.*
+import com.lynbrookrobotics.kapuchin.timing.Priority.*
+import com.revrobotics.CANSparkMax
+import com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless
+import info.kunalsheth.units.generated.*
+
+class ClimberWinchHardware : SubsystemHardware<ClimberWinchHardware, ClimberWinchComponent>() {
+    override val period: Time = 30.Millisecond
+    override val name: String = "Climber Winch"
+    override val syncThreshold: Time = 20.Millisecond
+    override val priority: Priority = Low
+
+    val escConfiguration by escConfigPref()
+
+    private val winchMotorId by pref(14)
+    val winch by hardw { CANSparkMax(winchMotorId, kBrushless) }
+}
+
+class ClimberWinchComponent(hardware: ClimberWinchHardware) : Component<ClimberWinchComponent, ClimberWinchHardware, OffloadedOutput>(hardware) {
+    override val fallbackController: ClimberWinchComponent.(Time) -> OffloadedOutput = {
+        PercentOutput(hardware.escConfiguration, 0.Percent)
+    }
+
+    override fun ClimberWinchHardware.output(value: OffloadedOutput) {
+        value.writeTo(winch)
+    }
+}
