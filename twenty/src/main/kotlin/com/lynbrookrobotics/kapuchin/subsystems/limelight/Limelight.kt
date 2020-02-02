4ac7011f5b4fcac3784143c73becc3d80919f03c
sid <skninja6@gmail.com>
2020-02-01 18:20:52 -0800

    limelight.kt

---
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/limelight/Limelight.kt
@@ -0,0 +1,10 @@
+package com.lynbrookrobotics.kapuchin.subsystems.limelight
+
+import info.kunalsheth.units.generated.*
+
+data class LimelightReading(
+        val tx: Angle, val ty: Angle,
+        val tx0: Dimensionless, val ty0: Dimensionless,
+        val thor: Dimensionless, val tvert: Dimensionless,
+        val ta: Dimensionless
+)
