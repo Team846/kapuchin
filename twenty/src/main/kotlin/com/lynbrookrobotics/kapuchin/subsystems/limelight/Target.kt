86790c0d2d2fbf155e5c60f76cca3881659778c1
sid <skninja6@gmail.com>
2020-02-03 17:43:44 -0800

    added target

--- twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/limelight/Target.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/limelight/Target.kt
@@ -1,2 +1,7 @@
 package com.lynbrookrobotics.kapuchin.subsystems.limelight
 
+import com.lynbrookrobotics.kapuchin.control.data.*
+import info.kunalsheth.units.generated.*
+import com.lynbrookrobotics.kapuchin.preferences.*
+
+data class DetectedTarget(val innerGoalPos: Position?, val outerGoalPos: Position?)
