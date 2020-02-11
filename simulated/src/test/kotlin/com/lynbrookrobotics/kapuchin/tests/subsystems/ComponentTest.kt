f649a403116490e93ad7fb10bf54975689530954
Kunal Sheth <kunal@kunalsheth.info>
2020-02-10 21:07:13 -0800

    Delay Component controller executions to avoid null pointers on init

--- simulated/src/test/kotlin/com/lynbrookrobotics/kapuchin/tests/subsystems/ComponentTest.kt
+++ simulated/src/test/kotlin/com/lynbrookrobotics/kapuchin/tests/subsystems/ComponentTest.kt
@@ -3,6 +3,8 @@ package com.lynbrookrobotics.kapuchin.tests.subsystems
 import com.lynbrookrobotics.kapuchin.tests.*
 import com.lynbrookrobotics.kapuchin.timing.*
 import com.lynbrookrobotics.kapuchin.timing.clock.*
+import info.kunalsheth.units.generated.*
+import info.kunalsheth.units.math.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
@@ -12,9 +14,10 @@ class ComponentTest {
     private class ComponentTestElc : TC<ComponentTestElc, ComponentTestHardw>(ComponentTestHardw(), EventLoop)
     private class ComponentTestHardw : TSH<ComponentTestHardw, ComponentTestElc>("ComponentTest Hardware")
 
-    @Test(timeout = 1 * 1000)
+    @Test(timeout = 2 * 1000)
     fun `event loop components only update on event loop ticks`() = threadDumpOnFailure {
         val c = ComponentTestElc()
+        Thread.sleep(1000)
 
         val j = scope.launch { c.countTo(10) }
         while (c.routine == null) Thread.sleep(1)
