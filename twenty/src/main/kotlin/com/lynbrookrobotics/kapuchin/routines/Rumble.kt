a64457caea3a3f224ecf8380fe2d68bd83dda081
Andy Min <andrewkmin@gmail.com>
2020-02-13 17:42:23 -0800

    Prep for merge

--- twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/routines/Rumble.kt
+++
@@ -1,9 +0,0 @@
-package com.lynbrookrobotics.kapuchin.routines
-
-import com.lynbrookrobotics.kapuchin.subsystems.driver.*
-import info.kunalsheth.units.generated.*
-
-suspend fun RumbleComponent.set(rumble: Rumble) = startRoutine("Set") {
-    controller { rumble }
-}
-
