e2176c841eae16c6c63dcba1d6bb13c81ab3e1fd
Kunal Sheth <kunal@kunalsheth.info>
2020-02-16 04:52:49 -0800

    Cleanup intake slider subsystem

--- twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/intake/IntakeSlider.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/intake/IntakeSlider.kt
@@ -1,13 +1,13 @@
 package com.lynbrookrobotics.kapuchin.subsystems.intake
 
 import com.lynbrookrobotics.kapuchin.Subsystems.Companion.pneumaticTicker
+import com.lynbrookrobotics.kapuchin.Subsystems.Companion.sharedTickerTiming
 import com.lynbrookrobotics.kapuchin.hardware.*
 import com.lynbrookrobotics.kapuchin.subsystems.*
 import com.lynbrookrobotics.kapuchin.subsystems.intake.IntakeSliderState.*
 import com.lynbrookrobotics.kapuchin.timing.*
 import edu.wpi.first.wpilibj.Solenoid
 import info.kunalsheth.units.generated.*
-import info.kunalsheth.units.math.*
 
 enum class IntakeSliderState(val output: Boolean) { Out(true), In(false) }
 
@@ -15,17 +15,16 @@ class IntakeSliderComponent(hardware: IntakeSliderHardware) : Component<IntakeSl
 
     override val fallbackController: IntakeSliderComponent.(Time) -> IntakeSliderState = { In }
     override fun IntakeSliderHardware.output(value: IntakeSliderState) {
-        solenoid.set(value.output)
+        sliderSolenoid.set(value.output)
     }
 }
 
 class IntakeSliderHardware : SubsystemHardware<IntakeSliderHardware, IntakeSliderComponent>() {
-    override val period: Time = 100.milli(Second)
-    override val syncThreshold: Time = 20.milli(Second)
-    override val priority: Priority = Priority.Low
-    override val name: String = "Intake Slider"
+    override val period = sharedTickerTiming()
+    override val syncThreshold = sharedTickerTiming()
+    override val priority = Priority.Low
+    override val name = "Intake Slider"
 
-    private val solenoidPort = 3
-
-    val solenoid by hardw { Solenoid(solenoidPort) }
+    private val sliderSolenoidPort = 3
+    val sliderSolenoid by hardw { Solenoid(sliderSolenoidPort) }
 }
