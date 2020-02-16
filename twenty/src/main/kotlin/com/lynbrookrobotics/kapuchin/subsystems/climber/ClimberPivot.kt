52169c23ec1f0928c0289b11f48998858198eaa4
Kunal Sheth <kunal@kunalsheth.info>
2020-02-16 02:02:06 -0800

    Cleanup climber pivot subsystem

--- twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/climber/ClimberPivot.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/climber/ClimberPivot.kt
@@ -1,13 +1,13 @@
 package com.lynbrookrobotics.kapuchin.subsystems.climber
 
 import com.lynbrookrobotics.kapuchin.Subsystems.Companion.pneumaticTicker
+import com.lynbrookrobotics.kapuchin.Subsystems.Companion.sharedTickerTiming
 import com.lynbrookrobotics.kapuchin.hardware.*
 import com.lynbrookrobotics.kapuchin.subsystems.*
 import com.lynbrookrobotics.kapuchin.subsystems.climber.ClimberPivotState.*
 import com.lynbrookrobotics.kapuchin.timing.*
 import edu.wpi.first.wpilibj.Solenoid
 import info.kunalsheth.units.generated.*
-import info.kunalsheth.units.math.*
 
 enum class ClimberPivotState(val output: Boolean) { Down(false), Up(true) }
 
@@ -16,17 +16,16 @@ class ClimberPivotComponent(hardware: ClimberPivotHardware) : Component<ClimberP
     override val fallbackController: ClimberPivotComponent.(Time) -> ClimberPivotState = { Down }
 
     override fun ClimberPivotHardware.output(value: ClimberPivotState) {
-        solenoid.set(value.output)
+        pivotSolenoid.set(value.output)
     }
 }
 
 class ClimberPivotHardware : SubsystemHardware<ClimberPivotHardware, ClimberPivotComponent>() {
-    override val period: Time = 50.milli(Second)
-    override val syncThreshold: Time = 20.milli(Second)
-    override val priority: Priority = Priority.Low
-    override val name: String = "Climber Pivot"
+    override val period = sharedTickerTiming()
+    override val syncThreshold = sharedTickerTiming()
+    override val priority = Priority.Low
+    override val name = "Climber Pivot"
 
-    private val solenoidId = 1
-
-    val solenoid by hardw { Solenoid(solenoidId) }
+    private val pivotSolenoidChannel = 1
+    val pivotSolenoid by hardw { Solenoid(pivotSolenoidChannel) }
 }
