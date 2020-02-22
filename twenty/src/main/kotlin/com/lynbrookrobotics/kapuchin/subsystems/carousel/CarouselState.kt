39e1047b919a0e85cb0665cce6274e54f745e8e9
Kunal Sheth <kunal@kunalsheth.info>
2020-02-22 06:55:05 -0800

    Unit test CarouselState

--- twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/carousel/CarouselState.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/subsystems/carousel/CarouselState.kt
@@ -3,10 +3,9 @@ package com.lynbrookrobotics.kapuchin.subsystems.carousel
 import com.lynbrookrobotics.kapuchin.logging.*
 import com.lynbrookrobotics.kapuchin.logging.Level.*
 import info.kunalsheth.units.generated.*
-import info.kunalsheth.units.math.*
 import kotlin.math.roundToInt
 
-class CarouselState(component: CarouselComponent) : Named by Named("State", component) {
+class CarouselState(component: Named) : Named by Named("State", component) {
     private val internal = arrayOf(false, false, false, false, false)
 
     private fun index(robotBearing: Angle) =
@@ -15,23 +14,24 @@ class CarouselState(component: CarouselComponent) : Named by Named("State", comp
     operator fun get(robotBearing: Angle) = internal.get(index(robotBearing))
 
     operator fun set(robotBearing: Angle, state: Boolean) {
-        log(Warning) { "Setting state of slot #${robotBearing.CarouselSlot withDecimals 1} to $state" }
-        internal.set(index(robotBearing), state)
+        val index = index(robotBearing)
+        log(Debug) { "Setting state of slot #$index @ ${robotBearing.Degree withDecimals 0}˚ from ${internal.get(index)} to $state" }
+        if (internal[index] && state) log(Error) { "#$index was assumed to be full. Setting to full again." }
+        internal.set(index, state)
     }
 
-    private fun iterateAround(slot: Angle) = (0..size / 2)
-            .map { it.CarouselSlot }
-            .flatMap { setOf(slot + it, slot - it) }
+    private fun closest(slot: Angle, bias: Boolean, f: (Angle) -> Boolean): `∠`? {
+        val signum = if (bias) -1 else 1
+        for (i in 0..2) {
+            val offset = i.CarouselSlot * signum
+            if (f(slot + offset)) return slot + offset
+            if (f(slot - offset)) return slot - offset
+        }
+        return null
+    }
 
-    fun closestEmpty(robotBearing: Angle) = iterateAround(robotBearing)
-            .filterNot { get(it) }
-            .minBy { it.abs }
-            ?.roundToInt(CarouselSlot)
-
-    fun closestFull(robotBearing: Angle) = iterateAround(robotBearing)
-            .filter { get(it) }
-            .minBy { it.abs }
-            ?.roundToInt(CarouselSlot)
+    fun closestEmpty(robotBearing: Angle) = closest(robotBearing, false) { !get(it) }
+    fun closestFull(robotBearing: Angle) = closest(robotBearing, true) { get(it) }
 
     val ammo get() = internal.count { it }
     val size = internal.size
