0c231ea9f81f0f53287fedffcbda6b74b7f2843d
Andy Min <andrewkmin@gmail.com>
2020-02-24 02:42:13 -0800

    Update field dimensions

--- twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/Field.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/kapuchin/Field.kt
@@ -5,8 +5,9 @@ import com.lynbrookrobotics.kapuchin.preferences.*
 import info.kunalsheth.units.generated.*
 
 object Field : Named by Named("Field") {
+    // TODO UPDATE ON NETWORK TABLES
     val targetDiameter by pref(30, Inch) // "diameter" of the inscribed circle of the outer goal
-    val innerGoalDepth by pref(25.25, Inch) // Distance between outer and inner goal
+    val innerGoalDepth by pref(29.25, Inch) // Distance between outer and inner goal
     val targetHeight by pref(98.25, Inch) // height from floor to center of outer goal
     val ballMass by pref(0.141748, Kilogram)
     val ballDiameter by pref(7, Inch)
