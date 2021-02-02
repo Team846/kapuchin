5f094a30e617ad4fcc18ef2ff82f4a6e6e754450
Andrew (Andy) Min <26666500+akmin04@users.noreply.github.com>
2021-02-02 02:10:10 -0800

    Fix test packages

---
+++ architecture/src/commonTest/kotlin/com/lynbrookrobotics/kapuchin/control/math/DelayTest.kt
@@ -0,0 +1,21 @@
+package com.lynbrookrobotics.kapuchin.control.math
+
+import com.lynbrookrobotics.kapuchin.*
+import com.lynbrookrobotics.kapuchin.control.data.*
+import kotlin.test.Test
+
+class DelayTest {
+
+    @Test
+    fun `Delay delays by the previous lookback values`() {
+        anyInt.filter { it > 0 }.forEach { falloff ->
+            val delay = delay<Int>(falloff)
+            val retained = falloff - 1
+            repeat(retained) { delay(it) `is equal to?` null }
+            repeat(retained) { it `is equal to?` delay(it) }
+            repeat(retained) { it `is equal to?` delay(-it) }
+            repeat(retained) { -it `is equal to?` delay(0) }
+            repeat(retained) { 0 `is equal to?` delay(it) }
+        }
+    }
+}
---
+++ architecture/src/commonTest/kotlin/com/lynbrookrobotics/kapuchin/control/math/DerivativeTest.kt
@@ -0,0 +1,31 @@
+package com.lynbrookrobotics.kapuchin.control.math
+
+import com.lynbrookrobotics.kapuchin.*
+import info.kunalsheth.units.generated.*
+import kotlin.test.Test
+
+class DerivativeTest {
+    @Test
+    fun `Derivative of constant is zero`() {
+        anyDouble.forEach { constant ->
+            val differentiator = differentiator(
+                ::p,
+                -Second, constant.Foot
+            )
+            repeat(50) { time ->
+                differentiator(time.Second, constant.Foot) `is equal to?` 0.FootPerSecond
+            }
+        }
+    }
+
+    @Test
+    fun `Derivative of a increasing numbers is greater than zero`() {
+        val differentiator = differentiator(
+            ::p,
+            -Second, (anyDouble.min()!! - 1).Foot
+        )
+        anyDouble.sorted().forEachIndexed { time, increasingValue ->
+            differentiator(time.Second, increasingValue.Foot) `is greater than?` 0.FootPerSecond
+        }
+    }
+}
