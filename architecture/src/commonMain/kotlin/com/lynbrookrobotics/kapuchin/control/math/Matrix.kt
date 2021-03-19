7311dcb3de6f1582f68f0106795ed3a1fefcd24f
kaustubh <kaustubh.khulbe@gmail.com>
2021-03-19 00:35:13 -0700

    add and subtract for matrix

--- architecture/src/commonMain/kotlin/com/lynbrookrobotics/kapuchin/control/math/Matrix.kt
+++ architecture/src/commonMain/kotlin/com/lynbrookrobotics/kapuchin/control/math/Matrix.kt
@@ -3,27 +3,58 @@ package com.lynbrookrobotics.kapuchin.control.math
 import info.kunalsheth.units.generated.*
 
 class Matrix(num_rows: Int, num_cols: Int, matrix: Array<Array<T?>>){
-    var storage_matrix: Array<Array<T?>> = matrix
-    val num_rows = num_rows
-    val num_cols = num_cols
+    private var storage_matrix: Array<Array<T?>> = matrix
+    private val rows = num_rows
+    private val cols = num_cols
     init {
-        storage_matrix = Array<Array<T?>>(num_rows){ arrayOfNulls<T?>(num_cols) }
+        storage_matrix = Array<Array<T?>>(rows){ arrayOfNulls<T?>(cols) }
     }
 
-//    inline infix fun `+` (other: Matrix): Matrix? {
-//        if (num_cols != other.num_cols || num_rows != other.num_rows){
-//            return null
-//        }
-//        else
-//        {
-//            val ret = Matrix(num_rows, num_cols, storage_matrix)
-//            for (i in other.storage_matrix){
-//                for(j in i){
-//                    ret.storage_matrix[storage_matrix.indexOf(i)][i.indexOf(j)] =
-//                        storage_matrix[storage_matrix.indexOf(i)][storage_matrix[storage_matrix.indexOf(i)]?.indexOf(j)]
-//                    + other.storage_matrix[storage_matrix.indexOf(i)][storage_matrix[storage_matrix.indexOf(i)].indexOf(j)]
+    infix fun `+` (other: Matrix): Matrix? {
+        if(rows != other.rows || cols != other.cols) return null
+        val returnMatrix = Matrix(rows, cols, storage_matrix)
+
+        for(i in 0..storage_matrix.size){
+            for(j in 0..storage_matrix[i].size){
+                val otherVal = other.storage_matrix[i][j];
+                if(otherVal == null) returnMatrix.storage_matrix[i][j] = storage_matrix[i][j]
+                else returnMatrix.storage_matrix[i][j] = storage_matrix[i][j]?.plus(otherVal)
+            }
+        }
+        return returnMatrix
+    }
+
+    infix fun `-` (other: Matrix): Matrix? {
+        if(rows != other.rows || cols != other.cols) return null
+        val returnMatrix = Matrix(rows, cols, storage_matrix)
+
+        for(i in 0..storage_matrix.size){
+            for(j in 0..storage_matrix[i].size){
+                val otherVal = other.storage_matrix[i][j];
+                if(otherVal == null) returnMatrix.storage_matrix[i][j] = storage_matrix[i][j]
+                else returnMatrix.storage_matrix[i][j] = storage_matrix[i][j]?.minus(otherVal)
+            }
+        }
+        return returnMatrix
+    }
+
+//    infix fun `*` (other: Matrix): Matrix? {
+//        if(cols != other.rows) return null
+//
+//        val returnMatrix = Matrix(rows, other.cols, Array<Array<T?>>(cols){ arrayOfNulls<T?>(other.rows) })
+//
+//        for(i in 0 until rows){
+//            for(j in 0 until other.cols){
+//                for(k in 0..other.rows){
+//                    val otherVal = other.storage_matrix[i][j];
+//                    if(otherVal == null) returnMatrix.storage_matrix[i][j] = storage_matrix[i][j]
+//                    else returnMatrix.storage_matrix[i][j] = storage_matrix[i][j]?.times(otherVal)
 //                }
 //            }
 //        }
+//
+//        return returnMatrix
 //    }
+
 }
+
