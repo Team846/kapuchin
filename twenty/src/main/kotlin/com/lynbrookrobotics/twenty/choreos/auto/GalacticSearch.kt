15f4eafd84324eb2eb1e743320593528944b84a2
kaustubh <kaustubh.khulbe@gmail.com>
2021-03-23 20:08:16 -0700

    network tables fixed hopefully

--- twenty/src/main/kotlin/com/lynbrookrobotics/twenty/choreos/auto/GalacticSearch.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/twenty/choreos/auto/GalacticSearch.kt
@@ -1,45 +1,40 @@
 package com.lynbrookrobotics.twenty.choreos.auto
 
+import com.lynbrookrobotics.kapuchin.control.data.*
+import com.lynbrookrobotics.kapuchin.control.math.drivetrain.*
+import com.lynbrookrobotics.kapuchin.logging.*
+import com.lynbrookrobotics.kapuchin.logging.Level.*
 import com.lynbrookrobotics.kapuchin.routines.*
 import com.lynbrookrobotics.twenty.*
+import com.lynbrookrobotics.twenty.Subsystems
+import com.lynbrookrobotics.twenty.choreos.intakeBalls
+import com.lynbrookrobotics.twenty.routines.*
+import com.lynbrookrobotics.twenty.routines.followTrajectory
 import com.lynbrookrobotics.twenty.subsystems.drivetrain.*
 import com.lynbrookrobotics.twenty.subsystems.intake.*
 import edu.wpi.first.networktables.NetworkTableInstance
 import info.kunalsheth.units.generated.*
 import info.kunalsheth.units.math.*
-import com.lynbrookrobotics.kapuchin.control.math.drivetrain.*
-import com.lynbrookrobotics.kapuchin.logging.*
-import com.lynbrookrobotics.kapuchin.logging.Level.*
-import com.lynbrookrobotics.twenty.routines.followTrajectory
-import com.lynbrookrobotics.twenty.routines.optimalEat
-import kotlinx.coroutines.launch
-import java.io.File
 import kotlin.math.log
-import kotlin.system.measureTimeMillis
-
-import com.lynbrookrobotics.kapuchin.control.data.*
-import com.lynbrookrobotics.kapuchin.logging.*
-import com.lynbrookrobotics.kapuchin.logging.Level.*
-import com.lynbrookrobotics.kapuchin.routines.*
-import com.lynbrookrobotics.twenty.Subsystems
-import com.lynbrookrobotics.twenty.choreos.intakeBalls
-import com.lynbrookrobotics.twenty.routines.*
-import com.lynbrookrobotics.twenty.subsystems.shooter.ShooterHoodState.Up
-import info.kunalsheth.units.generated.*
-import info.kunalsheth.units.math.*
-import kotlinx.coroutines.cancelChildren
-import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
-import kotlin.system.measureTimeMillis
 
-
-private fun getPathToRun(): Int{
+private fun getPathToRun(): Int {
     NetworkTableInstance.getDefault().getTable("/SmartDashboard").getEntry("path").setNumber(0)
-    var path =  NetworkTableInstance.getDefault().getTable("/SmartDashboard").getEntry("path").toDouble().toInt()
+    var path =
+            NetworkTableInstance.getDefault()
+                    .getTable("/SmartDashboard")
+                    .getEntry("path")
+                    .toString()
+                    .toInt()
 
-    while(path == 0){ //waits for jetson to detect red
+    while (path == 0) { // waits for jetson to detect red
         println("Waiting for Jetson to run")
-        path =  NetworkTableInstance.getDefault().getTable("/SmartDashboard").getEntry("path").toInt()
+        path =
+                NetworkTableInstance.getDefault()
+                        .getTable("/SmartDashboard")
+                        .getEntry("path")
+                        .toString()
+                        .toInt()
     }
     return path
 }
@@ -49,23 +44,21 @@ private fun getPathToRun(): Int{
  *
  * @author Kaustubh Khulbe
  */
-suspend fun Subsystems.galacticSearch(){
+suspend fun Subsystems.galacticSearch() {
     var pathName = ""
-    //val speedFactor = 20.Percent
-    //val carouselAngle by carousel.hardware.position.readEagerly().withoutStamps
+    // val speedFactor = 20.Percent
+    // val carouselAngle by carousel.hardware.position.readEagerly().withoutStamps
 
-    startChoreo("Galactic Search"){
+    startChoreo("Galactic Search") {
         choreography {
-
             carousel.rezero()
-            val pathRun = getPathToRun();
+            val pathRun = getPathToRun()
             val intakeJob = launch { intakeBalls() }
 
             if (pathRun == 1) pathName = "GalacticSearch_A_RED.tsv"
-            else if(pathRun == 2) pathName = "GalacticSearch_A_RED.tsv"
-            else if(pathRun == 3) pathName = "
-            else if(pathRun == 4) pathName = ""
-            
+            else if (pathRun == 2) pathName = "GalacticSearch_A_RED.tsv"
+            else if (pathRun == 3) pathName = "" else if (pathRun == 4) pathName = ""
+
             val path = loadRobotPath(pathName)
             if (path == null) {
                 log(Error) { "Unable to find $pathName" }
@@ -73,15 +66,13 @@ suspend fun Subsystems.galacticSearch(){
 
             path?.let {
                 drivetrain.followTrajectory(
-                    fastAsFuckPath(it, drivetrain.speedFactor),
-                    maxExtrapolate = drivetrain.maxExtrapolate,
-                    reverse = true
-                )
+                        fastAsFuckPath(it, drivetrain.speedFactor),
+                        maxExtrapolate = drivetrain.maxExtrapolate,
+                        reverse = true)
             }
 
             intakeJob.cancel()
         }
-
     }
-    //High level function for path
+    // High level function for path
 }
