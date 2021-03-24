3725f888d053906e1e57982137e8c8a2bed0e040
Ankith Madadi <ankithm28@gmail.com>
2021-03-23 21:42:06 -0700

    Smart Dashboard.get

--- twenty/src/main/kotlin/com/lynbrookrobotics/twenty/choreos/auto/GalacticSearch.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/twenty/choreos/auto/GalacticSearch.kt
@@ -1,40 +1,28 @@
 package com.lynbrookrobotics.twenty.choreos.auto
 
-import com.lynbrookrobotics.kapuchin.control.data.*
-import com.lynbrookrobotics.kapuchin.control.math.drivetrain.*
 import com.lynbrookrobotics.kapuchin.logging.*
 import com.lynbrookrobotics.kapuchin.logging.Level.*
 import com.lynbrookrobotics.kapuchin.routines.*
-import com.lynbrookrobotics.twenty.*
 import com.lynbrookrobotics.twenty.Subsystems
 import com.lynbrookrobotics.twenty.choreos.intakeBalls
-import com.lynbrookrobotics.twenty.routines.*
 import com.lynbrookrobotics.twenty.routines.followTrajectory
-import com.lynbrookrobotics.twenty.subsystems.drivetrain.*
-import com.lynbrookrobotics.twenty.subsystems.intake.*
-import edu.wpi.first.networktables.NetworkTableInstance
-import info.kunalsheth.units.generated.*
-import info.kunalsheth.units.math.*
-import kotlin.math.log
+import com.lynbrookrobotics.twenty.routines.rezero
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import kotlinx.coroutines.launch
 
 private fun getPathToRun(): Int {
-    NetworkTableInstance.getDefault().getTable("/SmartDashboard").getEntry("path").setNumber(0)
-    var path =
-            NetworkTableInstance.getDefault()
-                    .getTable("/SmartDashboard")
-                    .getEntry("path")
-                    .toString()
-                    .toInt()
+    SmartDashboard.getEntry("path").setNumber(0)
+    var path = SmartDashboard
+        .getEntry("path")
+        .toString()
+        .toInt()
 
     while (path == 0) { // waits for jetson to detect red
         println("Waiting for Jetson to run")
-        path =
-                NetworkTableInstance.getDefault()
-                        .getTable("/SmartDashboard")
-                        .getEntry("path")
-                        .toString()
-                        .toInt()
+        path = SmartDashboard
+            .getEntry("path")
+            .toString()
+            .toInt()
     }
     return path
 }
@@ -66,9 +54,10 @@ suspend fun Subsystems.galacticSearch() {
 
             path?.let {
                 drivetrain.followTrajectory(
-                        fastAsFuckPath(it, drivetrain.speedFactor),
-                        maxExtrapolate = drivetrain.maxExtrapolate,
-                        reverse = true)
+                    fastAsFuckPath(it, drivetrain.speedFactor),
+                    maxExtrapolate = drivetrain.maxExtrapolate,
+                    reverse = true
+                )
             }
 
             intakeJob.cancel()
