6def6b4175b6bf202d26a63dc163430feb868cb2
kaustubh <kaustubh.khulbe@gmail.com>
2021-03-30 23:26:20 -0700

    fixed angle error in code

--- architecture/src/commonMain/kotlin/com/lynbrookrobotics/kapuchin/control/math/drivetrain/swerve/SwerveOdometry.kt
+++ architecture/src/commonMain/kotlin/com/lynbrookrobotics/kapuchin/control/math/drivetrain/swerve/SwerveOdometry.kt
@@ -6,18 +6,18 @@ import info.kunalsheth.units.generated.*
 import info.kunalsheth.units.math.*
 import kotlin.math.log
 
-class SwerveOdometry(startPos: Position, private val radius: Length) {
+class SwerveOdometry(val startPos: Position, private val radius: Length) {
     fun updatePosition(modules: Array<Pair<Length, Angle>>): Position?{
         if(modules.size != 4) return null
 
         var xDist: Length = 0.Inch
         var yDist: Length = 0.Inch
-//        var thetaDist: Dimensionless = 0.Radian
+        var thetaDist: Angle = 0.Radian
         modules.forEach{
             xDist += it.first * cos(it.second)
             yDist += it.first * cos(it.second)
-//            thetaDist += (it.first / radius)
+            thetaDist += (it.first / radius) * Radian
         }
-        return null
+        return startPos + Position(xDist, yDist, thetaDist)
     }
 }
