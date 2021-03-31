4b03edec0474266da19f3f20e8f2066ebd8c80ad
kaustubh <kaustubh.khulbe@gmail.com>
2021-03-31 00:28:04 -0700

    minor formatting improvements

--- architecture/src/commonMain/kotlin/com/lynbrookrobotics/kapuchin/control/math/drivetrain/swerve/SwerveOdometry.kt
+++ architecture/src/commonMain/kotlin/com/lynbrookrobotics/kapuchin/control/math/drivetrain/swerve/SwerveOdometry.kt
@@ -6,7 +6,20 @@ import info.kunalsheth.units.generated.*
 import info.kunalsheth.units.math.*
 import kotlin.math.log
 
-class SwerveOdometry(val startPos: Position, private val radius: Length) {
+/**
+ * Helper class for the swerve odometry algorithm
+ * @param pos --> initial position of the robot (most likely 0,0,0)
+ * @param radius --> axle length / 2 or just the radius of the robot
+ * @author Kaustubh
+ */
+class SwerveOdometry(var pos: Position, private val radius: Length) {
+
+    /**
+     * Updates the current `Position` of the swerve drive given the angle and how far each module has travelled
+     * @param modules --> array of size four, contains `Pair<Length, Angle>` where Length
+     * is distance traveled by the wheel and Angle is the current angle of the wheel
+     * @return Position --> new position of the robot given starting position and `modules`
+     */
     fun updatePosition(modules: Array<Pair<Length, Angle>>): Position?{
         if(modules.size != 4) return null
 
@@ -18,6 +31,7 @@ class SwerveOdometry(val startPos: Position, private val radius: Length) {
             yDist += it.first * cos(it.second)
             thetaDist += (it.first / radius) * Radian
         }
-        return startPos + Position(xDist, yDist, thetaDist)
+        pos += Position(xDist, yDist, thetaDist)
+        return pos
     }
 }
