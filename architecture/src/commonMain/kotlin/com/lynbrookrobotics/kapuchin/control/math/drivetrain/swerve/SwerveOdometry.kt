b0b2047e99ffb4c63635da41e311290ce7e942c9
kaustubh <kaustubh.khulbe@gmail.com>
2021-03-31 18:40:48 -0700

    changing odometry algorithm to actually work

--- architecture/src/commonMain/kotlin/com/lynbrookrobotics/kapuchin/control/math/drivetrain/swerve/SwerveOdometry.kt
+++ architecture/src/commonMain/kotlin/com/lynbrookrobotics/kapuchin/control/math/drivetrain/swerve/SwerveOdometry.kt
@@ -1,10 +1,8 @@
 package com.lynbrookrobotics.kapuchin.control.math.drivetrain.swerve
 
 import com.lynbrookrobotics.kapuchin.control.data.*
-import com.lynbrookrobotics.kapuchin.logging.Level.*
 import info.kunalsheth.units.generated.*
 import info.kunalsheth.units.math.*
-import kotlin.math.log
 
 /**
  * Helper class for the swerve odometry algorithm
@@ -12,7 +10,7 @@ import kotlin.math.log
  * @param radius --> axle length / 2 or just the radius of the robot
  * @author Kaustubh
  */
-class SwerveOdometry(var pos: Position, private val radius: Length) {
+class SwerveOdometry(var pos: Position, private val radius: Length, private val lenBy2: Length) {
 
     /**
      * Updates the current `Position` of the swerve drive given the angle and how far each module has travelled
@@ -20,18 +18,20 @@ class SwerveOdometry(var pos: Position, private val radius: Length) {
      * is distance traveled by the wheel and Angle is the current angle of the wheel
      * @return Position --> new position of the robot given starting position and `modules`
      */
-    fun updatePosition(modules: Array<Pair<Length, Angle>>): Position?{
-        if(modules.size != 4) return null
+    fun updatePosition(modules: Array<Pair<Vector<Length>, Angle>>): Position? {
+        val x_top =
+            (modules[0].first.x * cos(modules[0].second)) avg (modules[1].first.x * cos(modules[1].second)) // may need to be something other than avg
+        val x_bottom = (modules[2].first.x * cos(modules[2].second)) avg (modules[3].first.x * cos(modules[3].second))
 
-        var xDist: Length = 0.Inch
-        var yDist: Length = 0.Inch
-        var thetaDist: Angle = 0.Radian
-        modules.forEach{
-            xDist += it.first * cos(it.second)
-            yDist += it.first * cos(it.second)
-            thetaDist += (it.first / radius) * Radian
-        }
-        pos += Position(xDist, yDist, thetaDist)
-        return pos
+        val y_left = (modules[0].first.y * sin(modules[0].second)) avg (modules[2].first.y * sin(modules[2].second))
+        val y_right = (modules[1].first.y * sin(modules[1].second)) avg (modules[3].first.y * sin(modules[3].second))
+
+        return null //TODO(Fix this)
+    }
+
+    fun getPoint(dLeft: Length, dRight: Length) {
+        val dCenter = dLeft avg dRight
     }
 }
+
+private infix fun L1A0M0T0I0Theta0N0J0.avg(that: Length) = (this + that) / 2
