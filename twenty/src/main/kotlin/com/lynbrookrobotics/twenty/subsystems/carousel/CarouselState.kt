bb5c45b4ccbc599457273d84b7a660f0964fd197
Andrew (Andy) Min <me@andrewmin.info>
2021-10-19 03:18:35 -0700

    Add mutex to carousel state

--- twenty/src/main/kotlin/com/lynbrookrobotics/twenty/subsystems/carousel/CarouselState.kt
+++ twenty/src/main/kotlin/com/lynbrookrobotics/twenty/subsystems/carousel/CarouselState.kt
@@ -2,35 +2,30 @@ package com.lynbrookrobotics.twenty.subsystems.carousel
 
 import com.lynbrookrobotics.kapuchin.logging.*
 import info.kunalsheth.units.generated.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 // http://svn.lynbrookrobotics.com/cad21/trunk/Users/Andy%20Min/CarouselStatePositions.pdf
 class CarouselState(component: Named) : Named by Named("State", component) {
 
+    private val mutex = Mutex()
     var balls = 0
         private set
-    val maxBalls = 5
+    private val maxBalls = 5
 
-    fun intakeAngle(): Angle? {
+    suspend fun intakeAngle(): Angle? = mutex.withLock {
         if (balls == maxBalls) return null
         return balls.CarouselSlot
     }
 
-    fun shootInitialAngle(): Angle? {
+    suspend fun shootInitialAngle(): Angle? = mutex.withLock {
         if (balls == 0) return null
         return (balls - 1).CarouselSlot
     }
 
-    fun push(count: Int = 1) {
-        balls += count
-    }
+    suspend fun push(count: Int = 1) = mutex.withLock { balls += count }
 
-    fun pop() {
-        balls--
-    }
-
-    fun clear() {
-        balls = 0
-    }
+    suspend fun clear() = mutex.withLock { balls = 0 }
 
     override fun toString() = "CarouselState($balls/$maxBalls)"
 }
